d57aee128589a5306abdce38d587ace08805e77f
ni.bagherpour <ni.bagherpour@sadad.co.ir>
2024-08-13 10:51:51 +0330

    initial commit

---
+++ frontend/apps/portal/src/app/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.@emotion/styled file.
+   */
+  return (
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>
+              <span> Hello there, </span>
+              Welcome to Oxygen Portal 👋
+            </h1>
+          </div>
+        </div>
+      </div>
+  );
+}
