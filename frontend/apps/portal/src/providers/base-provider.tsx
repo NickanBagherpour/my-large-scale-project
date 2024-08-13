156192725ac65a02beb81fc33f013271a1b7cf5a
ni.bagherpour <ni.bagherpour@sadad.co.ir>
2024-08-13 18:21:38 +0330

    feat: add ThemeConfig provider to BaseProvider

--- frontend/apps/portal/src/providers/base-provider.tsx
+++ frontend/apps/portal/src/providers/base-provider.tsx
@@ -1,15 +1,26 @@
 'use client';
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeConfig } from '@oxygen-portal/ui-kit';
+import { IConfig } from '@oxygen-portal/types';
 
 type BaseProviderProps = {
   children: React.ReactNode;
 };
 
 const BaseProvider = ({ children }: BaseProviderProps) => {
+
+  const handleLocaleChange = (config: IConfig) => {
+    // changeLanguage(config.locale);
+  };
+
   return (
     <>
-      <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      <AppRouterCacheProvider>
+        <ThemeConfig onLocaleChange={handleLocaleChange}>
+        {children}
+        </ThemeConfig>
+      </AppRouterCacheProvider>
     </>
   );
 };
--- frontend/libs/ui-kit/src/theme/directionality.tsx
+++ frontend/libs/ui-kit/src/theme/directionality.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from 'react';
+
+import { prefixer } from 'stylis';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
 
 interface IDirectionalityProps {
   isRtl: boolean;
