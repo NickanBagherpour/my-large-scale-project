a1ccea9de864c6eb76f333996021682804532ff7
ni.bagherpour <ni.bagherpour@sadad.co.ir>
2024-08-14 10:36:35 +0330

    refactor: comment jsxImportSource from tsconfig.json and update main layout

--- frontend/apps/portal/src/app/page.tsx
+++ frontend/apps/portal/src/app/page.tsx
@@ -1,21 +1,24 @@
 'use client';
 
-// import { Box } from '@mui/material';
+import { Button, Box } from '@oxygen-portal/ui-kit';
 
 export default function Index() {
   return (
-    <div className='wrapper'>
-      <div className='container'>
-        <div id='welcome'>
+    <div className="wrapper">
+      <div className="container">
+        <div id="welcome">
           <h1>
             <span> Hello there, </span>
             Welcome to Oxygen Portal 👋
           </h1>
         </div>
 
-        {/*   <Box display={'flex'} marginY={'2rem'} color={'blue'}>
-            This is Material
-          </Box>*/}
+        <Box display={'flex'} marginY={'2rem'} color={'blue'}>
+          This is Material
+        </Box>
+
+        <Button variant={'contained'}>Click me</Button>
+
       </div>
     </div>
   );
