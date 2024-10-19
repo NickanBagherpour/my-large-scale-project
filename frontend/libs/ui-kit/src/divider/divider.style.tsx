d33eb1633d85faad0b21b578a692764b3d69c740
p.kazemabadi <P.Kazemabadi@sadad.co.ir>
2024-10-19 11:09:56 +0330

    refactor: refactor divider

--- frontend/libs/ui-kit/src/divider/divider.style.tsx
+++ frontend/libs/ui-kit/src/divider/divider.style.tsx
@@ -3,4 +3,6 @@ import { Divider as AntDivider } from 'antd';
 
 export const StyledDivider = styled(AntDivider)`
   border-color: ${(p) => p.theme.border._100};
+  display: flex;
+  flex-direction: row-reverse;
 `;
