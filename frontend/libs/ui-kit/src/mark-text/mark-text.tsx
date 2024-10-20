ea130a63a45a5a85182531e23ccde155ea9e418e
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-10-20 10:30:41 +0330

    feat: copied mark-text from cip

---
+++ frontend/libs/ui-kit/src/mark-text/mark-text.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import * as S from './mark-text.style';
+
+export type HighlightColorType = 'success' | 'warning' | 'error' | React.CSSProperties['color'];
+
+type PropsType = {
+  text: string;
+  wordToHighlight: string;
+  highlightColor: HighlightColorType;
+  // fontSize?: string;
+  // fontWeight?: string;
+  // [key: string]: any;
+};
+
+export const MarkText = ({ text, wordToHighlight, highlightColor, ...rest }: PropsType) => {
+  const parts = text.split(new RegExp(`(${wordToHighlight})`, 'gi'));
+  return (
+    <p>
+      {parts.map((part, index) =>
+        part === wordToHighlight ? (
+          <S.StyledSpan customStyle={highlightColor} key={index} {...rest}>
+            {part}
+          </S.StyledSpan>
+        ) : (
+          part
+        )
+      )}
+    </p>
+  );
+};
