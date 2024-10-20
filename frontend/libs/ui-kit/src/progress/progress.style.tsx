b4cd37d4d0f6ab39ba63237e4f6bd8414a148763
p.kazemabadi <P.Kazemabadi@sadad.co.ir>
2024-10-20 10:52:21 +0330

    refactor: refactor progress antd

--- frontend/libs/ui-kit/src/progress/progress.style.tsx
+++ frontend/libs/ui-kit/src/progress/progress.style.tsx
@@ -1,4 +1,21 @@
 import styled from 'styled-components';
 import { Progress as AntProgress } from 'antd';
 
-export const StyledProgress = styled(AntProgress)``;
+export const StyledProgress = styled(AntProgress)`
+  .ant-progress-outer {
+    display: flex;
+    flex-direction: row-reverse;
+
+    .ant-progress-text {
+      margin-right: 1rem;
+      font-family: 'IranSans', sans-serif;
+    }
+    .ant-progress-inner {
+      display: flex;
+      flex-direction: row-reverse;
+      .ant-progress-bg {
+        padding: 0.8rem;
+      }
+    }
+  }
+`;
