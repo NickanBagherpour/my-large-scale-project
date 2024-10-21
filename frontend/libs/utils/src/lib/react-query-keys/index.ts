dfb9cf71111b8ffca1ff8915d40d0e53498ea01b
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-10-21 10:29:25 +0330

    feat: init clients-list widget

--- frontend/libs/utils/src/lib/react-query-keys/index.ts
+++ frontend/libs/utils/src/lib/react-query-keys/index.ts
@@ -1,7 +1,9 @@
 /*--import--*/
+import { KEYS as CLIENTS_LIST_KEYS } from './widgets/clients-list.key';
 import { KEYS as REQUESTS_KEYS } from './widgets/requests.key';
 
 export const RQKEYS = {
   /*--export--*/
+  CLIENTS_LIST: CLIENTS_LIST_KEYS,
   REQUESTS: REQUESTS_KEYS,
 };
