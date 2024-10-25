aaf4ff044615b31880a6eb52ea7f91b304fe0f98
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-10-25 16:57:03 +0330

    feat: add a story for grid card

---
+++ frontend/libs/reusable-components/src/components/grid-card/grid-card.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import GridCard from './grid-card';
+
+const meta = {
+  title: 'Reusable-Components/GridCard',
+  component: GridCard,
+  argTypes: {
+    name: { control: 'text' },
+    englishName: { control: 'text' },
+    status: { control: 'select', options: ['active', 'inactive'] },
+    date: { control: 'text' },
+    href: { control: 'text' },
+    wordToHighlight: { control: 'text' },
+  },
+} satisfies Meta<typeof GridCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const UpstreamCard: Story = {
+  args: {
+    name: 'اپ بله',
+    englishName: 'englishName',
+    status: 'active',
+    date: '01/12/1403',
+    href: '/',
+    wordToHighlight: 'بله',
+  },
+};
+
+export const ClientCard: Story = {
+  args: {
+    href: '/',
+    name: 'API-SERVICES-UPSTREAM',
+    activeServersCount: 12,
+  },
+};
