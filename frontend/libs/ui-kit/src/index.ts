44f57a5a72ef6078a81bc4a93722a33fce407b6e
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-10-25 13:58:30 +0330

    feat: move the index file of media into its own folder

--- frontend/libs/ui-kit/src/index.ts
+++ frontend/libs/ui-kit/src/index.ts
@@ -1,6 +1,5 @@
 'use client';
-
-export * as Icons from './assets';
+export * as Icons from './assets/media';
 export { default as getAntTheme } from './theme/ant-theme';
 export { default as ThemeConfig } from './theme/theme-config';
 export * from './theme/theme';
