3d39f1f3dc4b1e199f35e647ba75dfabcc1e6fae
S.Salehpour <s.salehpour@sadad.co.ir>
2024-10-26 11:55:07 +0330

    feat: create client-history-widget

--- frontend/libs/utils/src/lib/react-query-keys/index.ts
+++ frontend/libs/utils/src/lib/react-query-keys/index.ts
@@ -1,10 +1,12 @@
 /*--import--*/
+import { KEYS as CLIENT_HISTORY_KEYS } from './widgets/client-history.key';
 import { KEYS as SERVICE_MANAGEMENT_KEYS } from './widgets/service-management.key';
 import { KEYS as CLIENTS_LIST_KEYS } from './widgets/clients-list.key';
 import { KEYS as REUSABLE_COMPONENTS_KEYS } from './widgets/reusable-components.key';
 
 export const RQKEYS = {
   /*--export--*/
+  CLIENT_HISTORY: CLIENT_HISTORY_KEYS,
   SERVICE_MANAGEMENT: SERVICE_MANAGEMENT_KEYS,
   CLIENTS_LIST: CLIENTS_LIST_KEYS,
   REUSABLE_COMPONENTS: REUSABLE_COMPONENTS_KEYS,
