518e0481b548fea85685013af64620bc4252da1f
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-10-31 10:24:02 +0330

    refactor: clean app.tsx

--- frontend/libs/widgets/client-details-widget/src/components/app/app.tsx
+++ frontend/libs/widgets/client-details-widget/src/components/app/app.tsx
@@ -3,30 +3,20 @@ import * as S from './app.style';
 import { useTr } from '@oxygen/translation';
 import ClientInfo from '../client-info/client-info';
 import Plugins from '../plugins/plugins';
+import Services from '../services/services';
 
 const App = () => {
   const [t] = useTr();
 
   const tabs = [
     { label: t('client_info'), key: '1', children: <ClientInfo /> },
-    { label: t('services'), key: '2', children: 'first tab' },
+    { label: t('services'), key: '2', children: <Services /> },
     { label: t('plugins'), key: '3', children: <Plugins /> },
   ];
 
   return (
     <Container title={'App-Bale'}>
-      <S.Tabs
-        type='line'
-        onChange={(e) => console.log(':)', e)}
-        activeKey='3'
-        items={tabs.map(({ label, key, children }) => {
-          return {
-            label,
-            key,
-            children,
-          };
-        })}
-      />
+      <S.Tabs type='line' activeKey='1' items={tabs} />
     </Container>
   );
 };
