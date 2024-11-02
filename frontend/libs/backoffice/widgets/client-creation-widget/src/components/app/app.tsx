c015b12cc22dc3329c97fa5ff590e927ea64f457
a.ghaffar <a.ghaffar@sadad.co.ir>
2024-11-02 09:25:50 +0330

    feat: initial the new structure

---
+++ frontend/libs/backoffice/widgets/client-creation-widget/src/components/app/app.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+import { useTr } from '@oxygen/translation';
+import { PageProps } from '@oxygen/types';
+
+import { useAppDispatch, useAppState } from '../../context';
+//import { useGetReportDataQuery } from '../../services';
+
+import FirstStep from '../first-step/first-step';
+import SecondStep from '../second-step/second-step';
+import ThirdStep from '../third-step/third-step';
+import FourthStep from '../fourth-step/fourth-step';
+
+import * as S from './app.style';
+
+type AppProps = PageProps & {
+  //
+};
+
+const App: React.FC<AppProps> = (props) => {
+  const dispatch = useAppDispatch();
+  const state = useAppState();
+  const [t] = useTr();
+  const [currentStep, setCurrentStep] = useState(1);
+
+  /* Sample Query Usage
+  const { data, isFetching, isError } = useGetReportDataQuery(prepareParams());
+
+  function prepareParams() {
+     const { filters,submit,pagination,...rest } = state;
+     const params = {
+       form: submit,
+       pagination: pagination,
+     };
+
+     return params;
+   }
+ */
+  const stepsItem = [
+    { title: t('progress_bar.first_step'), Content: <FirstStep setCurrentStep={setCurrentStep} /> },
+    { title: t('progress_bar.second_step'), Content: <SecondStep setCurrentStep={setCurrentStep} /> },
+    { title: t('progress_bar.third_step'), Content: <ThirdStep /> },
+    { title: t('progress_bar.fourth_step'), Content: <FourthStep /> },
+  ];
+
+  return (
+    <S.AppContainer title={t('create_new_client')}>
+      <S.Steps items={stepsItem} current={currentStep} />
+      {stepsItem[currentStep].Content}
+    </S.AppContainer>
+  );
+};
+
+export default App;
