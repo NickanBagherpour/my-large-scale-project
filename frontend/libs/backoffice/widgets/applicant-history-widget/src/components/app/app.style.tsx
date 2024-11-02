193282263fd58c08ca090f5d4446502af135a3f5
S.Salehpour <s.salehpour@sadad.co.ir>
2024-11-02 15:48:50 +0330

    feat: create app component

--- frontend/libs/backoffice/widgets/applicant-history-widget/src/components/app/app.style.tsx
+++ frontend/libs/backoffice/widgets/applicant-history-widget/src/components/app/app.style.tsx
@@ -1,6 +1,47 @@
 import styled from 'styled-components';
+
 import { Container } from '@oxygen/ui-kit';
+import { cssVar, respondTo } from '@oxygen/utils';
 
 export const AppContainer = styled(Container)`
-  // background-color: pink;
+  height: 100%;
+
+  & .table-container {
+    flex: 1;
+  }
+`;
+
+export const FooterContainer = styled.div<any>`
+  display: flex;
+  flex-direction: row-reverse;
+  border-top: 1px solid ${(p) => p.theme.border._300};
+  margin-right: 1.5rem;
+  padding: 1.6rem;
+  gap: 1.6rem;
+  margin-bottom: 1.5rem;
+  background-color: ${(props) => props.theme.background.main};
+  flex: 0 0 auto;
+
+  @media print {
+    display: none;
+  }
+
+  ${respondTo.down('xs')} {
+    flex-direction: column;
+  }
+
+  ${respondTo.down('lg')} {
+    width: calc(100% - 3rem);
+  }
+
+  & .ant-btn.return-button {
+    padding: 0.8rem 1.6rem;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 22px;
+    text-align: center;
+    border-color: ${(props) => props.theme.border.main};
+    min-width: 9.6rem;
+  }
 `;
--- frontend/libs/backoffice/widgets/applicant-history-widget/src/components/app/app.tsx
+++ frontend/libs/backoffice/widgets/applicant-history-widget/src/components/app/app.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import { Box, Button } from '@oxygen/ui-kit';
 import { useTr } from '@oxygen/translation';
-import { PageProps } from '@oxygen/types';
+import { Nullable, PageProps } from '@oxygen/types';
+import { GlobalErrorContainer } from '@oxygen/reusable-components';
 
-import { useAppDispatch, useAppState } from '../../context';
-//import { useGetReportDataQuery } from '../../services';
+import { resetErrorMessageAction, updateClientIdAction, useAppDispatch, useAppState } from '../../context';
+import DataList from '../data-list/data-list';
 
 import * as S from './app.style';
 
@@ -17,21 +21,46 @@ const App: React.FC<AppProps> = (props) => {
   const state = useAppState();
   const [t] = useTr();
 
-  /* Sample Query Usage
-  const { data, isFetching, isError } = useGetReportDataQuery(prepareParams());
+  const searchParams = useSearchParams();
+  const clientId: Nullable<string> = searchParams.get('clientId');
 
-  function prepareParams() {
-     const { filters,submit,pagination,...rest } = state;
-     const params = {
-       form: submit,
-       pagination: pagination,
-     };
+  useEffect(() => {
+    // console.log('clientId: ', clientId);
+    updateClientIdAction(dispatch, clientId);
+  }, [clientId]);
 
-     return params;
-   }
- */
+  const router = useRouter();
+  const handleReturn = () => {
+    router.back();
+  };
 
-  return <S.AppContainer title={'ApplicantHistoryWidget'}>ApplicantHistoryWidget</S.AppContainer>;
+  return (
+    <S.AppContainer fillContainer={true} title={t('widget_name')}>
+      {/*render widget name based on clientId*/}
+      <GlobalErrorContainer
+        containerProps={{ marginBottom: '2.4rem' }}
+        errorMessage={state.errorMessage}
+        onClose={() => {
+          resetErrorMessageAction(dispatch);
+        }}
+      />
+      <Box className={'table-container'}>
+        {/*{clientId ? <DataList /> : <NoResult isLoading={false} />}*/}
+        <DataList />
+      </Box>
+      <S.FooterContainer>
+        <Button
+          className={'return-button'}
+          variant={'outlined'}
+          color={'primary'}
+          size={'large'}
+          onClick={handleReturn}
+        >
+          {t('button.return')}
+        </Button>
+      </S.FooterContainer>
+    </S.AppContainer>
+  );
 };
 
 export default App;
