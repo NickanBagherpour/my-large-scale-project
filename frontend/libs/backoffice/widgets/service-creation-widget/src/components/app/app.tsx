8bf5fec0343fd66c2fb733438a24c4f1dcf24c8a
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-11-02 14:23:32 +0330

    feat: init service-creation widget

---
+++ frontend/libs/backoffice/widgets/service-creation-widget/src/components/app/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './app.style';
+
+const App: React.FC = (_) => {
+  return <S.AppContainer title={'ServiceCreationWidget'}>ServiceCreationWidget</S.AppContainer>;
+};
+
+export default App;
---
+++ frontend/libs/backoffice/widgets/service-creation-widget/src/context/reducer.ts
@@ -0,0 +1,16 @@
+import { WidgetActionType, WidgetStateType } from './types';
+
+export const initialStateValue: WidgetStateType = {
+  errorMessage: null,
+};
+
+export const reducer = (state: WidgetStateType, action: WidgetActionType): WidgetStateType | undefined => {
+  switch (action.type) {
+    case 'UPDATE_GLOBAL_ERROR_MESSAGE': {
+      state.errorMessage = action.payload;
+      return;
+    }
+    default:
+      throw new Error(`this action type is not supported => ${action['type']}`);
+  }
+};
