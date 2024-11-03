dc5b72ce3540efff8abc5763213243d7ce00aa0e
p.kazemabadi <P.Kazemabadi@sadad.co.ir>
2024-11-03 09:22:14 +0330

    refactor: refactor zod syntax

--- frontend/libs/backoffice/widgets/edit-applicant-info-widget/src/types/settings.schema.ts
+++ frontend/libs/backoffice/widgets/edit-applicant-info-widget/src/types/settings.schema.ts
@@ -1,29 +1,27 @@
 import { z } from 'zod';
 import { FORM_ITEM_NAMES } from '../utils/form-item-name';
-import { createSchemaFieldRule } from 'antd-zod';
 import { i18nBase } from '@oxygen/translation';
 
-export const FormSchema = z.object({
-  [FORM_ITEM_NAMES.userName]: z
-    .string({ required_error: i18nBase.t('error.required') })
-    .min(1, { message: i18nBase.t('error.required') }),
-  [FORM_ITEM_NAMES.nationalCode]: z
-    .string({ required_error: i18nBase.t('error.required') })
-    .min(1, { message: i18nBase.t('error.required') }),
-  [FORM_ITEM_NAMES.organizationName]: z
-    .string({ required_error: i18nBase.t('error.required') })
-    .min(1, { message: i18nBase.t('error.required') }),
-  [FORM_ITEM_NAMES.mobileNumber]: z
-    .string({ required_error: i18nBase.t('error.required') })
-    .min(1, { message: i18nBase.t('error.required') }),
-  [FORM_ITEM_NAMES.telePhone]: z
-    .string({ required_error: i18nBase.t('error.required') })
-    .min(1, { message: i18nBase.t('error.required') }),
-  [FORM_ITEM_NAMES.email]: z
-    .string({ required_error: i18nBase.t('error.required') })
-    .min(1, { message: i18nBase.t('error.required') }),
-});
+export const FormSchema = (t: (key: string) => string) =>
+  z.object({
+    [FORM_ITEM_NAMES.userName]: z
+      .string({ required_error: i18nBase.t('error.required') })
+      .min(1, { message: i18nBase.t('error.required') }),
+    [FORM_ITEM_NAMES.nationalCode]: z
+      .string({ required_error: i18nBase.t('error.required') })
+      .min(1, { message: i18nBase.t('error.required') }),
+    [FORM_ITEM_NAMES.organizationName]: z
+      .string({ required_error: i18nBase.t('error.required') })
+      .min(1, { message: i18nBase.t('error.required') }),
+    [FORM_ITEM_NAMES.mobileNumber]: z
+      .string({ required_error: i18nBase.t('error.required') })
+      .min(1, { message: i18nBase.t('error.required') }),
+    [FORM_ITEM_NAMES.telePhone]: z
+      .string({ required_error: i18nBase.t('error.required') })
+      .min(1, { message: i18nBase.t('error.required') }),
+    [FORM_ITEM_NAMES.email]: z
+      .string({ required_error: i18nBase.t('error.required') })
+      .min(1, { message: i18nBase.t('error.required') }),
+  });
 
-export type FormValues = z.infer<typeof FormSchema>;
-
-export const rule = createSchemaFieldRule(FormSchema);
+export type FormValues = z.infer<ReturnType<typeof FormSchema>>;
--- frontend/libs/backoffice/widgets/edit-client-info-widget/src/types/settings.schema.ts
+++ frontend/libs/backoffice/widgets/edit-client-info-widget/src/types/settings.schema.ts
@@ -1,7 +1,5 @@
 import { z } from 'zod';
 import { FORM_ITEM_NAMES } from '../utils/form-item-name';
-import { createSchemaFieldRule } from 'antd-zod';
-import { i18nBase } from '@oxygen/translation';
 
 export const createFormSchema = (t: (key: string) => string) =>
   z.object({
@@ -10,15 +8,31 @@ export const createFormSchema = (t: (key: string) => string) =>
     [FORM_ITEM_NAMES.latinNameClient]: z
       .string({ required_error: t('error.required') })
       .min(1, { message: t('error.required') }),
-    [FORM_ITEM_NAMES.persianNameClient]: z.string({ required_error: t('error.required') }),
-    [FORM_ITEM_NAMES.clientType]: z.string({ required_error: t('error.required') }),
-    [FORM_ITEM_NAMES.clientId]: z.string({ required_error: t('error.required') }),
-    [FORM_ITEM_NAMES.identityAuth]: z.string({ required_error: t('error.required') }),
-    [FORM_ITEM_NAMES.websiteUrl]: z.string({ required_error: t('error.required') }),
-    [FORM_ITEM_NAMES.inputAddress]: z.string({ required_error: t('error.required') }),
-    [FORM_ITEM_NAMES.returnAddress]: z.string({ required_error: t('error.required') }),
+    [FORM_ITEM_NAMES.persianNameClient]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
+    [FORM_ITEM_NAMES.clientType]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
+    [FORM_ITEM_NAMES.clientId]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
+    [FORM_ITEM_NAMES.identityAuth]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
+    [FORM_ITEM_NAMES.websiteUrl]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
+    [FORM_ITEM_NAMES.inputAddress]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
+    [FORM_ITEM_NAMES.returnAddress]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
     [FORM_ITEM_NAMES.aggregatorStatus]: z.boolean().optional(),
-    [FORM_ITEM_NAMES.aggregator]: z.string({ required_error: t('error.required') }),
+    [FORM_ITEM_NAMES.aggregator]: z
+      .string({ required_error: t('error.required') })
+      .min(1, { message: t('error.required') }),
   });
 
 export type FormValues = z.infer<ReturnType<typeof createFormSchema>>;
