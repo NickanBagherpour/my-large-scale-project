d830d4d47404825e6b12844ab1d1dc1fd1704416
m.jafari <M.Jafari@sadad.co.ir>
2024-11-09 14:31:35 +0330

    fix: modify type in context of upstream-details widget

--- frontend/libs/backoffice/widgets/upstream-details-widget/src/context/types.ts
+++ frontend/libs/backoffice/widgets/upstream-details-widget/src/context/types.ts
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { FormFieldsType } from '../types';
+import { ModalFormFieldsType } from '../types';
 import { MessageType, Nullable } from '@oxygen/types';
 
-export type FiltersType = FormFieldsType;
+export type FiltersType = ModalFormFieldsType;
 
-export type SimpleFilters = Pick<FormFieldsType, 'code' | 'branchCode'>;
+export type SimpleFilters = Pick<ModalFormFieldsType, 'code' | 'branchCode'>;
 
 export type PaginationType = {
   page: number;
