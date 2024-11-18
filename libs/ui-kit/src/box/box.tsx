d787feefb8798d0224c72c0b0223b8887d91350c
p.kazemabadi <P.Kazemabadi@sadad.co.ir>
2024-11-18 15:03:29 +0330

    refactor: refactor box in uikit

--- libs/ui-kit/src/box/box.tsx
+++ libs/ui-kit/src/box/box.tsx
@@ -1,111 +1,95 @@
-import React, { ComponentProps, CSSProperties } from 'react';
+import React, { CSSProperties } from 'react';
 import styled, { css } from 'styled-components';
 
 interface TransientBoxProps {
-  $display?: CSSProperties['display'];
-  $flexDirection?: CSSProperties['flexDirection'];
-  $justifyContent?: CSSProperties['justifyContent'];
-  $alignItems?: CSSProperties['alignItems'];
-  $alignContent?: CSSProperties['alignContent'];
-  $flexWrap?: CSSProperties['flexWrap'];
-  $flexGrow?: CSSProperties['flexGrow'];
-  $flexShrink?: CSSProperties['flexShrink'];
-  $flexBasis?: CSSProperties['flexBasis'];
-  $alignSelf?: CSSProperties['alignSelf'];
-  $height?: CSSProperties['height'];
-  $maxHeight?: CSSProperties['maxHeight'];
-  $minHeight?: CSSProperties['minHeight'];
-  $width?: CSSProperties['width'];
-  $maxWidth?: CSSProperties['maxWidth'];
-  $minWidth?: CSSProperties['minWidth'];
-  $padding?: CSSProperties['padding'];
-  $paddingTop?: CSSProperties['paddingTop'];
-  $paddingRight?: CSSProperties['paddingRight'];
-  $paddingBottom?: CSSProperties['paddingBottom'];
-  $paddingLeft?: CSSProperties['paddingLeft'];
-  $margin?: CSSProperties['margin'];
-  $marginTop?: CSSProperties['marginTop'];
-  $marginRight?: CSSProperties['marginRight'];
-  $marginBottom?: CSSProperties['marginBottom'];
-  $marginLeft?: CSSProperties['marginLeft'];
-  $border?: CSSProperties['border'];
-  $borderRadius?: CSSProperties['borderRadius'];
-  $boxShadow?: CSSProperties['boxShadow'];
-  $backgroundColor?: CSSProperties['backgroundColor'];
-  $color?: CSSProperties['color'];
-  $fontSize?: CSSProperties['fontSize'];
-  $fontWeight?: CSSProperties['fontWeight'];
-  $lineHeight?: CSSProperties['lineHeight'];
-  $letterSpacing?: CSSProperties['letterSpacing'];
-  $textAlign?: CSSProperties['textAlign'];
-  $fill_children?: string;
+  display?: CSSProperties['display'];
+  flexDirection?: CSSProperties['flexDirection'];
+  justifyContent?: CSSProperties['justifyContent'];
+  alignItems?: CSSProperties['alignItems'];
+  alignContent?: CSSProperties['alignContent'];
+  flexWrap?: CSSProperties['flexWrap'];
+  flexGrow?: CSSProperties['flexGrow'];
+  flexShrink?: CSSProperties['flexShrink'];
+  flexBasis?: CSSProperties['flexBasis'];
+  alignSelf?: CSSProperties['alignSelf'];
+  height?: CSSProperties['height'];
+  maxHeight?: CSSProperties['maxHeight'];
+  minHeight?: CSSProperties['minHeight'];
+  width?: CSSProperties['width'];
+  maxWidth?: CSSProperties['maxWidth'];
+  minWidth?: CSSProperties['minWidth'];
+  padding?: CSSProperties['padding'];
+  paddingTop?: CSSProperties['paddingTop'];
+  paddingRight?: CSSProperties['paddingRight'];
+  paddingBottom?: CSSProperties['paddingBottom'];
+  paddingLeft?: CSSProperties['paddingLeft'];
+  margin?: CSSProperties['margin'];
+  marginTop?: CSSProperties['marginTop'];
+  marginRight?: CSSProperties['marginRight'];
+  marginBottom?: CSSProperties['marginBottom'];
+  marginLeft?: CSSProperties['marginLeft'];
+  border?: CSSProperties['border'];
+  borderRadius?: CSSProperties['borderRadius'];
+  boxShadow?: CSSProperties['boxShadow'];
+  backgroundColor?: CSSProperties['backgroundColor'];
+  color?: CSSProperties['color'];
+  fontSize?: CSSProperties['fontSize'];
+  fontWeight?: CSSProperties['fontWeight'];
+  lineHeight?: CSSProperties['lineHeight'];
+  letterSpacing?: CSSProperties['letterSpacing'];
+  textAlign?: CSSProperties['textAlign'];
+  fill_children?: string;
 }
 
 const StyledBox = styled.div<TransientBoxProps>`
-  display: ${(props) => props.$display ?? 'flex'};
-  flex-direction: ${(props) => props.$flexDirection ?? 'row'};
-  justify-content: ${(props) => props.$justifyContent ?? 'flex-start'};
-  align-items: ${(props) => props.$alignItems ?? 'stretch'};
-  align-content: ${(props) => props.$alignContent ?? 'stretch'};
-  flex-wrap: ${(props) => props.$flexWrap ?? 'nowrap'};
-  flex-grow: ${(props) => props.$flexGrow ?? 0};
-  flex-shrink: ${(props) => props.$flexShrink ?? 1};
-  flex-basis: ${(props) => props.$flexBasis ?? 'auto'};
-  align-self: ${(props) => props.$alignSelf ?? 'auto'};
-  height: ${(props) => props.$height ?? 'auto'};
-  max-height: ${(props) => props.$maxHeight ?? 'none'};
-  min-height: ${(props) => props.$minHeight ?? 'auto'};
-  width: ${(props) => props.$width ?? 'auto'};
-  max-width: ${(props) => props.$maxWidth ?? 'none'};
-  min-width: ${(props) => props.$minWidth ?? 'auto'};
-  padding: ${(props) => props.$padding ?? 0};
-  margin: ${(props) => props.$margin ?? 0};
-  border: ${(props) => props.$border ?? 'none'};
-  border-radius: ${(props) => props.$borderRadius ?? '0'};
-  box-shadow: ${(props) => props.$boxShadow ?? 'none'};
-  background-color: ${(props) => props.$backgroundColor ?? 'transparent'};
-  color: ${(props) => props.$color ?? 'inherit'};
-  font-size: ${(props) => props.$fontSize ?? 'inherit'};
-  font-weight: ${(props) => props.$fontWeight ?? 'normal'};
-  line-height: ${(props) => props.$lineHeight ?? 'normal'};
-  letter-spacing: ${(props) => props.$letterSpacing ?? 'normal'};
-  text-align: ${(props) => props.$textAlign ?? 'inherit'};
-
-  ${(props) => props.$paddingTop && `padding-top: ${props.$paddingTop};`}
-  ${(props) => props.$paddingRight && `padding-right: ${props.$paddingRight};`}
-  ${(props) => props.$paddingBottom && `padding-bottom: ${props.$paddingBottom};`}
-  ${(props) => props.$paddingLeft && `padding-left: ${props.$paddingLeft};`}
-  ${(props) => props.$marginTop && `margin-top: ${props.$marginTop};`}
-  ${(props) => props.$marginRight && `margin-right: ${props.$marginRight};`}
-  ${(props) => props.$marginBottom && `margin-bottom: ${props.$marginBottom};`}
-  ${(props) => props.$marginLeft && `margin-left: ${props.$marginLeft};`}
-
+  ${(props) => props.flexGrow && `flex-grow: ${props.flexGrow ?? 0}`};
+  ${(props) => props.flexWrap && `flex-wrap: ${props.flexWrap ?? 'nowrap'}`};
+  ${(props) => props.flexShrink && `flex-shrink: ${props.flexShrink ?? 1}`};
+  ${(props) => props.flexBasis && `flex-basis: ${props.flexBasis ?? 'auto'}`};
+  ${(props) => props.alignSelf && `align-self: ${props.alignSelf ?? 'auto'}`};
+  ${(props) => props.height && `height: ${props.height ?? 'auto'}`};
+  ${(props) => props.maxHeight && `max-height: ${props.maxHeight ?? 'none'}`};
+  ${(props) => props.minHeight && `min-height: ${props.minHeight ?? 'auto'}`};
+  ${(props) => props.width && `width: ${props.width ?? 'auto'}`};
+  ${(props) => props.maxWidth && `max-width: ${props.maxWidth ?? 'none'}`};
+  ${(props) => props.minWidth && `min-width: ${props.minWidth ?? 'auto'}`};
+  ${(props) => props.padding && `padding: ${props.padding ?? 0}`};
+  ${(props) => props.margin && `margin: ${props.margin ?? 0}`};
+  ${(props) => props.border && `border: ${props.border ?? 'none'}`};
+  ${(props) => props.borderRadius && `border-radius: ${props.borderRadius ?? '0'}`};
+  ${(props) => props.boxShadow && `box-shadow: ${props.boxShadow ?? 'none'}`};
+  ${(props) => props.backgroundColor && `background-color: ${props.backgroundColor ?? 'transparent'}`};
+  ${(props) => props.color && `color: ${props.color ?? 'inherit'}`};
+  ${(props) => props.fontSize && `font-size: ${props.fontSize ?? 'inherit'}`};
+  ${(props) => props.fontWeight && `font-weight: ${props.fontWeight ?? 'normal'}`};
+  ${(props) => props.lineHeight && `line-height: ${props.lineHeight ?? 'normal'}`};
+  ${(props) => props.letterSpacing && `letter-spacing: ${props.letterSpacing ?? 'normal'}`};
+  ${(props) => props.textAlign && `text-align: ${props.textAlign ?? 'inherit'}`};
   ${(props) =>
-    props.$fill_children === 'true'
-      ? css`
+    props.fill_children === 'true'
+      ? `
           & > * {
             width: 100%;
           }
         `
-      : css`
+      : `
           & > * {
             width: fit-content;
           }
         `}
 `;
 
-interface BoxProps {
+export interface BoxProps extends TransientBoxProps {
   visible?: boolean;
   children?: React.ReactNode;
   fillChildren?: boolean;
-  styleProps?: TransientBoxProps;
   className?: string;
 }
 
-export const Box: React.FC<BoxProps> = ({ visible = true, children, fillChildren = true, styleProps, className }) => {
+export const Box: React.FC<BoxProps> = ({ visible = true, children, fillChildren = true, className }) => {
   return (
     visible && (
-      <StyledBox {...styleProps} className={className} $fill_children={String(fillChildren)}>
+      <StyledBox className={className} fill_children={String(fillChildren)}>
         {children}
       </StyledBox>
     )
