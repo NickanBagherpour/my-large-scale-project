32cc3898cba926b20f169e0bd418ec341b93d29c
p.kazemabadi <P.Kazemabadi@sadad.co.ir>
2024-11-18 11:48:49 +0330

    fix: resolve chip uikit bug

--- libs/ui-kit/src/chip/chip.tsx
+++ libs/ui-kit/src/chip/chip.tsx
@@ -28,7 +28,7 @@ export const Chip = (props: ChipProps) => {
 
   const renderChip = (_ellipsis = true) => {
     return (
-      <S.StyledChip type={type} iconProp={iconProp} {...rest}>
+      <S.StyledChip type={type} {...rest}>
         {type === 'active' && iconProp && <i className={iconProp} />}
         <S.ChipContainer ellipsis={String(_ellipsis)}>{children}</S.ChipContainer>
       </S.StyledChip>
