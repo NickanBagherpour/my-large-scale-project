8213ef406d60305ecaf83b764fc8822ee2b8bf2d
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-11-21 08:10:57 +0330

    feat: add background image to the landing page

--- libs/customer/widgets/landing-widget/src/components/app/app.tsx
+++ libs/customer/widgets/landing-widget/src/components/app/app.tsx
@@ -1,18 +1,23 @@
+import Customers from '../customers/customers';
 import Faq from '../faq/faq';
 import HeroSection from '../hero-section/hero-section';
 import JoinUs from '../join-us/join-us';
 import Vision from '../vision/vision';
 import Why from '../why/why';
+import * as S from './app.style';
 
 const App = () => {
   return (
-    <>
-      <HeroSection />
-      <Why />
-      <JoinUs />
-      <Vision />
-      <Faq />
-    </>
+    <S.Main>
+      <S.WithBg>
+        <HeroSection />
+        <Why />
+        <JoinUs />
+        <Vision />
+        <Customers />
+        <Faq />
+      </S.WithBg>
+    </S.Main>
   );
 };
 
