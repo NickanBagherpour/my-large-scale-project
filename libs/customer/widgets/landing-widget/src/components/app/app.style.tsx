69351ebc77e40075dfbe50384bd1fffa00dcd243
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-11-22 08:18:27 +0330

    feat: add bg-color to with-bg component and remove Main component

--- libs/customer/widgets/landing-widget/src/components/app/app.style.tsx
+++ libs/customer/widgets/landing-widget/src/components/app/app.style.tsx
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
 import looper from 'apps/customer-portal/public/assets/images/looper.svg';
 
-export const Main = styled.div`
-  background: ${(p) => p.theme.background._50};
-`;
-
 export const WithBg = styled.div`
+  background: ${(p) => p.theme.background._50};
   background-image: url(${looper});
-  background-repeat: none;
+  background-repeat: no-repeat;
   background-size: cover;
 `;
