91710a17e2ae9479720db76676a9036568206640
S.Salehpour <s.salehpour@sadad.co.ir>
2024-11-22 15:07:42 +0330

    refactor: landing-layout, dashboard-layout, public-layout

--- apps/customer-portal/src/app/(public)/layout.tsx
+++ apps/customer-portal/src/app/(public)/layout.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { LandingLayout } from '@oxygen/customer/layouts';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return <LandingLayout>{children}</LandingLayout>;
 }
