97c25ad7ec90b5f2ff98197b122fd0613c5c52f1
S.Salehpour <s.salehpour@sadad.co.ir>
2024-11-22 11:29:47 +0330

    refactor: change header-subtitle font-size

--- libs/backoffice/widgets/upstream-list-widget/src/components/app/app.style.tsx
+++ libs/backoffice/widgets/upstream-list-widget/src/components/app/app.style.tsx
@@ -4,4 +4,8 @@ import { Container } from '@oxygen/ui-kit';
 
 export const UpstreamContainer = styled(Container)`
   padding-bottom: 2rem;
+
+  & .header__subtitle {
+    font-size: 1.2rem;
+  }
 `;
