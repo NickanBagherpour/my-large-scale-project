8961f5061638c9165dd3fd8dd4bb5479c9c02efe
p.kazemabadi <P.Kazemabadi@sadad.co.ir>
2024-11-27 14:38:32 +0330

    feat: add typoGraphy to uikit

--- libs/hooks/src/use-auth/use-auth.tsx
+++ libs/hooks/src/use-auth/use-auth.tsx
@@ -3,11 +3,10 @@
 import { useRouter } from 'next/navigation';
 
 import React, { createContext, useContext, useMemo } from 'react';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, signOut } from 'next-auth/react';
 
 import { LocalStorageKey } from '@oxygen/types';
-import { clearAllCookies, clearLocalStorageExceptForKey } from '@oxygen/utils';
-import { client } from '@oxygen/client';
+import { clearLocalStorageExceptForKey } from '@oxygen/utils';
 
 import useLocalStorage from '../use-local-storage/use-local-storage';
 
@@ -25,7 +24,7 @@ type AuthProviderProps = {
 
 const AuthProvider = (props: AuthProviderProps) => {
   // const [user, setUser, removeUser] = useLocalStorage<any>(LocalStorageKey.USER, null);
-  const user = useSession()?.data?.user;
+  const user = /*useSession()?.data?.user*/ true;
   const [userPhoto, setUserPhoto, removeUserPhoto] = useLocalStorage(LocalStorageKey.USER_PHOTO, null);
   const [, setMenu, removeMenus] = useLocalStorage(LocalStorageKey.MENU);
   const router = useRouter();
@@ -78,17 +77,17 @@ const AuthProvider = (props: AuthProviderProps) => {
   };
 
   function isAuth(): boolean {
-    return !!user //&& isTokenValid();
+    return !!user; //&& isTokenValid();
   }
 
- /* const isTokenValid = () => {
-    const tokenExpiryDate = useSession().data?.expires;
-    if (tokenExpiryDate && tokenExpiryDate < Date.now()) {
-      return false;
-    } else {
-      return true;
-    }
-  };*/
+  /* const isTokenValid = () => {
+     const tokenExpiryDate = useSession().data?.expires;
+     if (tokenExpiryDate && tokenExpiryDate < Date.now()) {
+       return false;
+     } else {
+       return true;
+     }
+   };*/
 
   const value = useMemo(
     () => ({
@@ -101,7 +100,7 @@ const AuthProvider = (props: AuthProviderProps) => {
       setUserPhoto,
       removeUserPhoto,
     }),
-    [JSON.stringify(user), userPhoto],
+    [JSON.stringify(user), userPhoto]
   );
   return <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>;
 };
