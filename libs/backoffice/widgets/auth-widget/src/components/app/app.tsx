e1995cf8d373f4baaade170186de1e226df9b561
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-12-01 11:25:54 +0330

    feat: add login button

--- libs/backoffice/widgets/auth-widget/src/components/app/app.tsx
+++ libs/backoffice/widgets/auth-widget/src/components/app/app.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
-
-import { useTr } from '@oxygen/translation';
 import { PageProps } from '@oxygen/types';
-
-import { useAppDispatch, useAppState } from '../../context';
-//import { useGetReportDataQuery } from '../../services';
-
 import * as S from './app.style';
+import { Button } from '@oxygen/ui-kit';
 
-type AppProps = PageProps & {
-  //
-};
+const SSO_URL =
+  'http://185.135.30.10:9443/identity/oauth2/auth/authorize?response_type=code&client_id=oxygen-portal-sd-client&scope=batch-user-info&state=xyz-_123&sso=1&redirect_uri=http%3A%2F%2F192.168.52.138%3A3000%2Fauth';
 
-const App: React.FC<AppProps> = (props) => {
-  const dispatch = useAppDispatch();
-  const state = useAppState();
-  const [t] = useTr();
-
-  /* Sample Query Usage
-  const { data, isFetching, isError } = useGetReportDataQuery(prepareParams());
-
-  function prepareParams() {
-     const { filters,submit,pagination,...rest } = state;
-     const params = {
-       form: submit,
-       pagination: pagination,
-     };
-
-     return params;
-   }
- */
-
-  return <S.AppContainer title={'AuthWidget'}>AuthWidget</S.AppContainer>;
+const App: React.FC<PageProps> = (_) => {
+  return (
+    <S.AppContainer title={'AuthWidget'}>
+      <Button href={SSO_URL}>Login</Button>
+    </S.AppContainer>
+  );
 };
 
 export default App;
