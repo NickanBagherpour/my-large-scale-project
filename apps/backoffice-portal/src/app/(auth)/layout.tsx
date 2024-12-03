9fef359058c952d2540eac7e0b3f8034733b6ca2
ni.bagherpour <ni.bagherpour@sadad.co.ir>
2024-12-03 08:53:40 +0330

    feat: work on auth

---
+++ apps/backoffice-portal/src/app/(auth)/layout.tsx
@@ -0,0 +1,6 @@
+import { AuthLayout } from '@oxygen/backoffice/layouts';
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+
+  return <AuthLayout>{children}</AuthLayout>;
+}
---
+++ apps/backoffice-portal/src/app/api/auth/signout/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { CookieKey } from '@oxygen/types';
+
+export async function GET(req: Request) {
+
+  const sessionTicket = cookies().get(CookieKey.SESSION_TICKET)?.value;
+  const token = cookies().get(CookieKey.SESSION_ID)?.value;
+  const url = `${process.env.NEXT_PUBLIC_SSO_URL}/identity/oauvth2/auth/session/signout?sessionTicket=${sessionTicket}`;
+
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+
+    console.log('ssssssssssssssssiiiiiiiiiiiiiiiiiggggggggnnnnnnnnnnnn', response);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch token');
+    }
+    const data = await response.json();
+    console.log('Received token:', data);
+
+    return new NextResponse(JSON.stringify({ success: true }));
+  } catch (error) {
+    console.error('Error during SSO:', error);
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+  }
+}
