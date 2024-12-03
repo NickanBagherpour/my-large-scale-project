3209661064fb95fa0ba1e7ac115bdfd9d44cfa91
a.ghaffar <a.ghaffar@sadad.co.ir>
2024-12-03 11:14:19 +0330

    conflict: resolve signin and signout error type

--- apps/backoffice-portal/src/app/api/auth/signout/route.ts
+++ apps/backoffice-portal/src/app/api/auth/signout/route.ts
@@ -3,18 +3,16 @@ import { cookies } from 'next/headers';
 import { CookieKey } from '@oxygen/types';
 
 export async function GET(req: Request) {
-
   const sessionTicket = cookies().get(CookieKey.SESSION_TICKET)?.value;
   const token = cookies().get(CookieKey.SESSION_ID)?.value;
   const url = `${process.env.NEXT_PUBLIC_SSO_URL}/identity/oauvth2/auth/session/signout?sessionTicket=${sessionTicket}`;
-
 
   try {
     const response = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     });
 
@@ -27,7 +25,8 @@ export async function GET(req: Request) {
     console.log('Received token:', data);
 
     return new NextResponse(JSON.stringify({ success: true }));
-  } catch (error) {
+  } catch (error: any) {
+    //add any type cuse build error
     console.error('Error during SSO:', error);
     return NextResponse.json({ success: false, error: error.message }, { status: 500 });
   }
