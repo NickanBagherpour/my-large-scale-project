15593a694ae5382e9f1f68a7195f7bcb4042279e
m.jafari <M.Jafari@sadad.co.ir>
2024-12-09 11:02:25 +0330

    fix: revert to lazy

--- libs/reusable-components/src/components/animation-loader/lazy-lottie.tsx
+++ libs/reusable-components/src/components/animation-loader/lazy-lottie.tsx
@@ -1,20 +1,12 @@
 import { Skeleton } from 'antd';
 import { type LottieComponentProps } from 'lottie-react';
 import { JSXElementConstructor, LazyExoticComponent, ReactElement, Suspense, lazy } from 'react';
-import dynamic from 'next//dynamic';
 
-let LazyLottieComponent: any; // LazyExoticComponent<
-//   (props: LottieComponentProps) => ReactElement<any, string | JSXElementConstructor<any>>
-// >;
+let LazyLottieComponent: LazyExoticComponent<
+  (props: LottieComponentProps) => ReactElement<any, string | JSXElementConstructor<any>>
+>;
 if (typeof document !== 'undefined') {
-  // LazyLottieComponent = lazy(() => import('lottie-react'));
-  LazyLottieComponent = dynamic(() => import('lottie-react'), {
-    loading: () => <p>Loading...</p>,
-    ssr: false,
-  });
-  // const DynamicHeader = dynamic(() => import('lottie-react'), {
-  //   loading: () => <p>Loading...</p>,
-  // })
+  LazyLottieComponent = lazy(() => import('lottie-react'));
 }
 
 interface LottieProps {
@@ -23,10 +15,10 @@ interface LottieProps {
 
 export default function LazyLottie({ id, ref, animationData, ...props }: LottieProps & LottieComponentProps) {
   return (
-    // <Suspense
-    //   fallback={<Skeleton.Node active={true} style={{ height: props.height, width: props.width, marginTop: '8rem' }} />}
-    // >
-    <LazyLottieComponent animationData={animationData} {...props} />
-    // </Suspense>
+    <Suspense
+      fallback={<Skeleton.Node active={true} style={{ height: props.height, width: props.width, marginTop: '8rem' }} />}
+    >
+      <LazyLottieComponent animationData={animationData} {...props} />
+    </Suspense>
   );
 }
