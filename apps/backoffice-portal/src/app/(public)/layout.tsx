4d2237aa72562c5aaf8e4be271566ac1b59bc23d
ni.bagherpour <ni.bagherpour@sadad.co.ir>
2024-12-10 16:58:37 +0330

    refactor: minor changes

--- apps/backoffice-portal/src/app/(public)/layout.tsx
+++ apps/backoffice-portal/src/app/(public)/layout.tsx
@@ -1,11 +1,7 @@
-'use client';
-
-import { ClientOnly } from '@oxygen/reusable-components';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <ClientOnly>{children}</ClientOnly>
+      {children}
     </div>
   );
 }
