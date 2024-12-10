4e27d06d81e1f7e80d639aa79c3da4fe0abf2af9
E.Tajik <E.Tajik@sadad.co.ir>
2024-12-10 09:39:57 +0330

    fix: fix push errors

--- libs/reusable-components/src/components/animation-loader/lazy-lottie.tsx
+++ libs/reusable-components/src/components/animation-loader/lazy-lottie.tsx
@@ -1,13 +1,9 @@
+'use client';
 import { Skeleton } from 'antd';
 import { type LottieComponentProps } from 'lottie-react';
-import { JSXElementConstructor, LazyExoticComponent, ReactElement, Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 
-let LazyLottieComponent: LazyExoticComponent<
-  (props: LottieComponentProps) => ReactElement<any, string | JSXElementConstructor<any>>
->;
-if (typeof document !== 'undefined') {
-  LazyLottieComponent = lazy(() => import('lottie-react'));
-}
+const LazyLottieComponent = lazy(() => import('lottie-react'));
 
 interface LottieProps {
   id?: string;
@@ -18,7 +14,7 @@ export default function LazyLottie({ id, ref, animationData, ...props }: LottieP
     <Suspense
       fallback={<Skeleton.Node active={true} style={{ height: props.height, width: props.width, marginTop: '8rem' }} />}
     >
-      <LazyLottieComponent animationData={animationData} {...props} />
+      {LazyLottieComponent && <LazyLottieComponent animationData={animationData} {...props} />}
     </Suspense>
   );
 }
