795c122d87dcec5dc180036d1f7fc967f6184470
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-12-16 08:11:32 +0330

    feat: change steps' state structure

--- libs/backoffice/widgets/service-creation-widget/src/context/reducer.ts
+++ libs/backoffice/widgets/service-creation-widget/src/context/reducer.ts
@@ -1,8 +1,15 @@
 import { steps } from '../components/app/app';
-import { WidgetActionType, WidgetStateType } from './types';
+import { StepIndex, WidgetActionType, WidgetStateType } from './types';
 
 export const initialStateValue: WidgetStateType = {
   step: 0,
+  stepStatuses: [
+    { name: 'generalInfo', status: 'error' },
+    { name: 'scope', status: 'finish' },
+    { name: 'upstream', status: 'error' },
+    { name: 'route', status: 'finish' },
+    { name: 'confirmData', status: 'finish' },
+  ],
   scopeMode: 'importFromSso',
   generalInfo: {
     tag: null,
@@ -29,8 +36,24 @@ export const initialStateValue: WidgetStateType = {
 
 export const reducer = (state: WidgetStateType, action: WidgetActionType): WidgetStateType | undefined => {
   switch (action.type) {
-    case 'NEXT_STEP':
-      return state.step < steps.length ? void state.step++ : undefined;
+    case 'NEXT_STEP': {
+      if (state.step < steps.length - 1) {
+        state.stepStatuses[state.step].status = 'finish';
+
+        const maybeNextStepIndex = state.stepStatuses.findIndex(
+          (item) => item.status === 'wait' || item.status === 'error'
+        );
+        const nextStepIndex = (maybeNextStepIndex !== -1 ? maybeNextStepIndex : steps.length - 1) as StepIndex;
+
+        state.step = nextStepIndex;
+        const nextStepStatus = state.stepStatuses[nextStepIndex].status;
+
+        if (nextStepStatus === 'wait') {
+          state.stepStatuses[maybeNextStepIndex].status = 'process';
+        }
+      }
+      return undefined;
+    }
 
     case 'PREVIOUS_STEP':
       return state.step > 0 ? void state.step-- : undefined;
--- libs/backoffice/widgets/service-creation-widget/src/context/types.ts
+++ libs/backoffice/widgets/service-creation-widget/src/context/types.ts
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { MessageType, Nullable } from '@oxygen/types';
 import { CreateScopeFormType, GeneralInfoValuesType, RouteType } from '../types';
 import { steps } from '../components/app/app';
+import { StepProps } from 'antd';
 
 // Partial<T>["length"] gives a union of all possible lengths of the array when elements are optional.
 // T["length"] gives the exact length of the original array.
@@ -9,9 +10,13 @@ import { steps } from '../components/app/app';
 type Indices<T extends readonly unknown[]> = Exclude<Partial<T>['length'], T['length']>;
 
 export type ScopeMode = 'importFromSso' | 'createScope';
+export type StepNames = 'generalInfo' | 'scope' | 'upstream' | 'route' | 'confirmData';
+export type StepIndex = Indices<typeof steps>;
+export type Statuses = StepProps['status'];
 
 export type WidgetStateType = {
-  step: Indices<typeof steps>;
+  step: StepIndex;
+  stepStatuses: Array<{ name: StepNames; status: Statuses }>;
   scopeMode: ScopeMode;
   generalInfo: GeneralInfoValuesType;
   scope: CreateScopeFormType;
