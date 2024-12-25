43542be7e09c0f4fa992dd56835f0278f5e549b2
e.tajik <e.tajik@sadad.co.ir>
2024-12-25 17:00:59 +0330

    fix: fix api changes resulting bugs

--- libs/backoffice/widgets/service-list-widget/src/services/api.ts
+++ libs/backoffice/widgets/service-list-widget/src/services/api.ts
@@ -1,8 +1,8 @@
 import Mockify from '@oxygen/mockify';
+import { client, portalUrl } from '@oxygen/client';
 
 import { ParamsType } from '../types';
-import { client, portalUrl } from '@oxygen/client';
-import { InquiryInfo, InquiryParams } from '../types/get-Inquiry-info.type';
+import { InquiryDto, InquiryParams } from '../types/get-Inquiry-info.type';
 const Api = {
   getServicesList: async (params: ParamsType) => {
     const res = Mockify.getServicesList(params);
@@ -14,7 +14,7 @@ const Api = {
   },
   getInquiryInfo: async (params: InquiryParams) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    return client.get<InquiryInfo>(`${portalUrl}/v1/services/service-inquiry-status`, { params });
+    return client.get<InquiryDto>(`${portalUrl}/v1/services/service-inquiry-status`, { params });
   },
   uploadService: async (serviceName: string) => {
     return client.post(`${portalUrl}v1/services/import-service`, { 'service-name': serviceName });
--- libs/backoffice/widgets/service-list-widget/src/utils/consts.ts
+++ libs/backoffice/widgets/service-list-widget/src/utils/consts.ts
@@ -1,3 +1,5 @@
+import { InquiryStatus } from '../types/get-Inquiry-info.type';
+
 export const AVAILABLE_ROWS_PER_PAGE = [5, 10, 25, 50, 100];
 export const INITIAL_ROW_PER_PAGE = AVAILABLE_ROWS_PER_PAGE[0];
 export const INITIAL_PAGE = 1;
@@ -7,4 +9,10 @@ export const UPLOAD_SERVICE_NAMES = {
 };
 export const SERVICE_NAME = {
   ServiceName: 'serviceName',
+};
+export const INQUIRY_STATUS: Record<1 | 2 | 3 | 4, InquiryStatus> = {
+  1: 'SERVICE_ALREADY_EXISTS',
+  2: 'SERVICE_IS_DRAFT',
+  3: 'SERVICE_NOT_FOUND',
+  4: 'SERVICE_EXISTS_IN_BAAM',
 };
