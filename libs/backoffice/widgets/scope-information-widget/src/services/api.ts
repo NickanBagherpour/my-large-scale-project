fc5bb0131ddad72efad8f593a9b8eb4e26ef1409
a.ghaffar <a.ghaffar@sadad.co.ir>
2024-12-28 16:28:35 +0330

    feat: replace get services data and reset nx

--- libs/backoffice/widgets/scope-information-widget/src/services/api.ts
+++ libs/backoffice/widgets/scope-information-widget/src/services/api.ts
@@ -7,7 +7,10 @@ const Api = {
   getReportData: async (params) => {
     return client.get<any>(`${portalUrl}/v1/scope/${params}`);
   },
-  getServicesData: async (params) => Mockify.getServices(params),
+  getServicesData: async (params) => {
+    const { page, pageSize, id } = params;
+    return client.get<any>(`${portalUrl}/v1/services?scope-id=${id}`, { params: { page, pageSize } });
+  },
   getExcel: async (params) => Mockify.getServices(params),
 };
 export default Api;
