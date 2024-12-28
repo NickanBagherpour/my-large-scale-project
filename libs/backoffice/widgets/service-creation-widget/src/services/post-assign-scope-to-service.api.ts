c5b4494212aa6072ffa92094698a43670b2552b7
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2024-12-28 15:08:29 +0330

    fix: invalidate get queries after form submission in each step

--- libs/backoffice/widgets/service-creation-widget/src/services/post-assign-scope-to-service.api.ts
+++ libs/backoffice/widgets/service-creation-widget/src/services/post-assign-scope-to-service.api.ts
@@ -1,16 +1,22 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AssignScopeToServiceParams } from '../types';
-import { updateMessageAction, useAppDispatch } from '../context';
+import { updateMessageAction, useAppDispatch, useAppState } from '../context';
 import Api from './api';
-import { ApiUtil } from '@oxygen/utils';
+import { ApiUtil, RQKEYS } from '@oxygen/utils';
 
 export const usePostAssignScopeToService = () => {
   const dispatch = useAppDispatch();
+  const queryClient = useQueryClient();
+  const { serviceName } = useAppState();
+
   return useMutation({
     mutationFn: (params: AssignScopeToServiceParams) => Api.postAssignScopeToService(params),
     onError: (e) => {
       const err = ApiUtil.getErrorMessage(e);
       updateMessageAction(dispatch, err);
+    },
+    async onSuccess() {
+      await queryClient.invalidateQueries({ queryKey: [RQKEYS.SERVICE_CREATION.SCOPE, serviceName] });
     },
   });
 };
