f84097c67983d3253b65097b1a48bfee6e43b904
m.jafari <M.Jafari@sadad.co.ir>
2024-12-29 11:43:54 +0330

    fix: remove no need console.log

--- libs/customer/widgets/auth-widget/src/services/verify-login-otp.api.ts
+++ libs/customer/widgets/auth-widget/src/services/verify-login-otp.api.ts
@@ -14,8 +14,6 @@ export const useVerifyLoginMutation = () => {
     mutationFn: (params: any) => Api.postVerifyLoginOTP(params),
     onSuccess: async (data) => {
       const sessionId = data?.headers['authorization'];
-
-      console.log('ssssssssssssssssssssssssse', sessionId, data.headers);
       setCookie(CookieKey.SESSION_ID, encrypt(sessionId), AUTH_SESSION_DURATION);
       return data;
     },
