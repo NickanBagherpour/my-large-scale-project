c1ffe37eccf263be6679ca9ff78a176497883894
a.ghaffar <a.ghaffar@sadad.co.ir>
2024-12-30 15:54:10 +0330

    feat: refactor the advance grid card

--- libs/reusable-components/src/components/advance-grid-card/advance-grid-card.tsx
+++ libs/reusable-components/src/components/advance-grid-card/advance-grid-card.tsx
@@ -1,141 +1,44 @@
 import React from 'react';
+import { Box } from '@oxygen/ui-kit';
+import { useTr } from '@oxygen/translation';
+import { AdvanceGridCardPropsType } from '../../utils/advance-grid-card/type';
+import { getStatusConfig } from '../../utils/advance-grid-card/consts';
 
 import * as S from './advance-grid-card.style';
-import { Box, Button } from '@oxygen/ui-kit';
-import { useTr } from '@oxygen/translation';
-import { text } from 'stream/consumers';
-
-interface SubmissionStatus {
-  code: number;
-  title: string;
-}
-
-interface Request {
-  requestId: number;
-  createDate: string; // Use 'string' if storing as text, or consider Date if parsed.
-  creator: number;
-  modifier: number;
-  trackCode: string;
-  submissionStatus: SubmissionStatus;
-  organizationId: number;
-  organizationName: string;
-  clientName: string;
-  permissionUserId: number;
-  aggregatorName: string;
-  serviceCount: number;
-}
-
-export type AdvanceGridCardPropsType = {
-  children?: React.ReactNode;
-  btnHandleClick: () => void;
-  btnLoading: boolean;
-  data: Request;
-};
-export const AdvanceGridCard = (props: AdvanceGridCardPropsType) => {
-  const { children, btnHandleClick, btnLoading, data } = props;
-  //hooks
+export const AdvanceGridCard = ({ btnHandleClick, btnLoading, data }: AdvanceGridCardPropsType) => {
   const [t] = useTr();
+  const { code } = data.submissionStatus;
 
-  let textColor;
-  let businessColor;
-  let businessIcone;
-  let bankColor;
-  let bankIcone;
-  let discriptionText;
-
-  const code = data.submissionStatus.code;
-  switch (code) {
-    case 2: {
-      textColor = 'warning';
-      businessColor = 'default';
-      businessIcone = <i className='icon-close' />;
-      bankColor = 'warning';
-      bankIcone = <i className='icon-close' />;
-      discriptionText = 'advance_grid_card.2';
-      break;
-    }
-    case 3: {
-      textColor = 'error';
-      bankColor = 'error';
-      bankIcone = <i className='icon-close' />;
-      businessColor = 'default';
-      businessIcone = <i className='icon-close' />;
-      discriptionText = 'advance_grid_card.3';
-      break;
-    }
-    case 4: {
-      textColor = 'warning';
-      bankColor = 'success';
-      bankIcone = <i className='icon-checkmark' />;
-      businessColor = 'warning';
-      businessIcone = <i className='icon-close' />;
-      discriptionText = 'advance_grid_card.4';
-      break;
-    }
-    case 5: {
-      textColor = 'warning';
-      bankColor = 'success';
-      bankIcone = <i className='icon-checkmark' />;
-      businessColor = 'warning';
-      businessIcone = <i className='icon-close' />;
-      discriptionText = 'advance_grid_card.5';
-      break;
-    }
-    case 6: {
-      textColor = 'error';
-      bankColor = 'success';
-      bankIcone = <i className='icon-checkmark' />;
-      businessColor = 'error';
-      businessIcone = <i className='icon-close' />;
-      discriptionText = 'advance_grid_card.6';
-      break;
-    }
-    case 7: {
-      textColor = 'success';
-      bankColor = 'success';
-      bankIcone = <i className='icon-checkmark' />;
-      businessColor = 'success';
-      businessIcone = <i className='icon-checkmark' />;
-      discriptionText = 'advance_grid_card.7';
-      break;
-    }
-    // Add more cases as needed
-    default: {
-      // Optional: Handle cases where no match is found
-      textColor = 'default';
-      businessColor = 'default';
-      businessIcone = '-';
-      bankColor = 'default';
-      bankIcone = '-';
-      discriptionText = '-';
-    }
-  }
+  // Get the status configuration based on the code
+  const { textColor, bankColor, bankIcon, businessColor, businessIcon, descriptionText } = getStatusConfig(code);
 
   return (
     <S.Container>
-      <S.Divider>
+      <S.Div>
         <S.Details>
           <S.Title>{data.clientName}</S.Title>
           <S.SubTitle>{data.aggregatorName}</S.SubTitle>
           <S.Date>{data.createDate}</S.Date>
-          <S.ServiceCount>{`${data.serviceCount}${t('advance_grid_card.on_demand_service')}`}</S.ServiceCount>
+          <S.ServiceCount>{`${data.serviceCount} ${t('advance_grid_card.on_demand_service')}`}</S.ServiceCount>
         </S.Details>
         <S.Status>
           <S.StatusContainer>
-            <S.Icon color={bankColor}>{bankIcone}</S.Icon>
+            <S.IconTop color={bankColor as S.ColorType}>{bankIcon}</S.IconTop>
             <S.Paragraph>{t('advance_grid_card.validation_commercial_banking')}</S.Paragraph>
           </S.StatusContainer>
-          <S.Line color={bankColor}></S.Line>
-          <S.Line color={businessColor}></S.Line>
+          <Box flexGrow={1}>
+            <S.LineUp color={bankColor as S.ColorType} />
+            <S.LineDown color={businessColor as S.ColorType} />
+          </Box>
           <S.StatusContainer>
-            <S.Icon color={businessColor}>{businessIcone}</S.Icon>
+            <S.IconButtom color={businessColor as S.ColorType}>{businessIcon}</S.IconButtom>
             <S.Paragraph>{t('advance_grid_card.business_unit')}</S.Paragraph>
           </S.StatusContainer>
         </S.Status>
-      </S.Divider>
-      <S.Discription color={textColor}>
+      </S.Div>
+      <S.Discription color={textColor as S.ColorType}>
         <span>{t('advance_grid_card.discription')}</span>
-        {t(discriptionText)}
+        {t(descriptionText)}
       </S.Discription>
       <S.Button variant='outlined' loading={btnLoading} onClick={btnHandleClick}>
         {t('button.view_request')}
