affc015efeffa94003ec4a802110d64290ead5f0
a.ghaffar <a.ghaffar@sadad.co.ir>
2024-12-30 15:39:51 +0330

    style: finish card style

--- libs/reusable-components/src/components/advance-grid-card/advance-grid-card.style.tsx
+++ libs/reusable-components/src/components/advance-grid-card/advance-grid-card.style.tsx
@@ -7,10 +7,12 @@ import { Button as UiKitButton } from '@oxygen/ui-kit';
 type ColorType = 'warning' | 'error' | 'success' | 'default';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   border-radius: 1.8rem;
   padding: 3rem 4rem;
   border: ${(p) => `1px solid ${p.theme.border._300}`};
-
   transition: all 200ms;
   overflow: hidden;
   background-image: url('${looperGroup}');
@@ -24,15 +26,18 @@ export const Button = styled(UiKitButton)`
   width: 100%;
 `;
 export const Discription = styled.div<{ color?: ColorType }>`
-  margin-bottom: 3rem;
+  margin: 2rem 0 3rem 0;
   padding: 0 1.2rem;
-
   span {
     color: inherit;
     font-size: 1.4rem;
     font-weight: 700;
   }
 
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 2.2rem;
   color: ${(p) =>
     ({
       warning: p.theme.warning.main,
@@ -41,21 +46,25 @@ export const Discription = styled.div<{ color?: ColorType }>`
       default: p.theme.text.tertiary,
     }[p.color || 'default'] || 'black')};
 `;
-
-export const Status = styled.div`
-  width: 100%;
-`;
-export const Details = styled.div`
-  width: 100%;
-  align-items: flex-end;
-`;
-export const Divider = styled.div`
+export const Div = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
   ${respondTo.down('sm')} {
     flex-direction: column;
   }
+`;
+
+export const Status = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 10rem;
+`;
+export const Details = styled.div`
+  width: 100%;
+  align-items: flex-end;
 `;
 
 export const Title = styled.p`
@@ -90,7 +99,9 @@ export const ServiceCount = styled.p`
   line-height: 1.8rem;
   margin: 1.2rem 0;
 `;
-export const Icon = styled.div<{ color: ColorType }>`
+export const IconTop = styled.div<{ color: ColorType }>`
+  position: absolute;
+  top: 0;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -107,10 +118,35 @@ export const Icon = styled.div<{ color: ColorType }>`
   i {
     color: ${(p) => p.theme.onPrimary};
     font-size: 2.4rem;
+    z-index: 100;
   }
 `;
-export const Line = styled.div<{ color: ColorType }>`
-  margin-left: 1.8rem;
+export const IconButtom = styled.div<{ color: ColorType }>`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(p) =>
+    ({
+      warning: p.theme.warning.main,
+      error: p.theme.error.main,
+      success: p.theme.success.main,
+      default: p.theme.text.tertiary,
+    }[p.color || 'default'] || 'black')};
+  border-radius: 10rem;
+  height: 4rem;
+  width: 4rem;
+  i {
+    color: ${(p) => p.theme.onPrimary};
+    font-size: 2.4rem;
+    z-index: 100;
+  }
+`;
+export const LineUp = styled.div<{ color: ColorType }>`
+  position: absolute;
+  top: 0;
+  margin-left: 1.9rem;
   background-color: ${(p) =>
     ({
       warning: p.theme.warning.main,
@@ -119,7 +155,21 @@ export const Line = styled.div<{ color: ColorType }>`
       default: p.theme.text.tertiary,
     }[p.color || 'default'] || 'black')};
   width: 2px;
-  height: 2.5rem;
+  height: 50%;
+`;
+export const LineDown = styled.div<{ color: ColorType }>`
+  position: absolute;
+  bottom: 0;
+  margin-left: 1.9rem;
+  background-color: ${(p) =>
+    ({
+      warning: p.theme.warning.main,
+      error: p.theme.error.main,
+      success: p.theme.success.main,
+      default: p.theme.text.tertiary,
+    }[p.color || 'default'] || 'black')};
+  width: 2px;
+  height: 50%;
 `;
 export const Paragraph = styled.p`
   color: ${(p) => p.theme.text.tertiary};
@@ -128,12 +178,11 @@ export const Paragraph = styled.p`
   font-style: normal;
   font-weight: 400;
   line-height: 2.2rem;
-  margin: 0;
+  margin: 1rem 0 1rem 5rem;
 `;
-export const StatusContainer = styled.p`
+export const StatusContainer = styled.div`
+  position: relative;
   margin: 0;
   padding: 0;
   display: flex;
-  align-items: center;
-  gap: 1.2rem;
 `;
