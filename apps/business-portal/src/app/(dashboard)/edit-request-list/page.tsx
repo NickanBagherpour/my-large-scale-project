1fb054bfc65e6d920f63b6bd401cafbbe62b627d
p.kazemabadi <P.Kazemabadi@sadad.co.ir>
2025-01-01 11:07:21 +0330

    feat: init edit request list

---
+++ apps/business-portal/src/app/(dashboard)/edit-request-list/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import EditRequestListWidget from '@oxygen/business/widgets/edit-request-list-widget';
+
+export default function Index(props) {
+  return <EditRequestListWidget parentProps={props} />;
+}
