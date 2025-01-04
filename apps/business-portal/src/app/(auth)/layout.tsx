7d1b66574f023f674911018b4d271b410da29cb8
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-01-04 09:20:44 +0330

    fix: add some 'use client' to components that use hooks

--- apps/business-portal/src/app/(auth)/layout.tsx
+++ apps/business-portal/src/app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import { AuthLayout } from '@oxygen/business/layouts';
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <AuthLayout>{children}</AuthLayout>;
 }
