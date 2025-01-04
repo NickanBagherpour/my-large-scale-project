0d012fcc20d729e898d8d9e3ebb474a9089f6d3e
r.sheikholmoluki <r.sheikholmoluki@sadad.co.ir>
2025-01-04 18:02:34 +0330

    fix: resolve errors

--- libs/backoffice/widgets/edit-route-widget/src/services/index.ts
+++ libs/backoffice/widgets/edit-route-widget/src/services/index.ts
@@ -1,5 +1,5 @@
 export { default as Api } from './api';
-export { useGetReportDataQuery } from './get-report.api';
+// export { useGetReportDataQuery } from './get-report.api';
 export { useGetRouteDetailsQuery } from './get-route-details.api';
 export { useGetServiceHttpMethod } from './get-service-http-method.api';
 export { useGetServiceProtocol } from './get-service-protocol.api';
