b6cf2fd4513154656fb8c775707a08da4adbec6d
r.sheikholmoluki <r.sheikholmoluki@sadad.co.ir>
2025-01-06 14:50:55 +0330

    fix: schema buid error

--- libs/backoffice/widgets/edit-route-widget/src/types/setting.schema.ts
+++ libs/backoffice/widgets/edit-route-widget/src/types/setting.schema.ts
@@ -4,7 +4,7 @@ import { REGEX_PATTERNS } from '@oxygen/utils';
 
 export const FormSchema = (t: (key: string) => string) =>
   z.object({
-    [FORM_ITEM_NAMES.action]: z.any({ required_error: t('error.required') }),
+    [FORM_ITEM_NAMES.method]: z.any({ required_error: t('error.required') }),
     [FORM_ITEM_NAMES.protocol]: z.any({ required_error: t('error.required') }),
     [FORM_ITEM_NAMES.path]: z.any(),
     // .string({ required_error: t('error.required') })
