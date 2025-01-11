59cac68777c06a7e91f812c92a2c4828c582684a
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-01-11 13:35:08 +0330

    feat: show step errors in status-modal

--- libs/backoffice/widgets/service-creation-widget/src/services/post-confirm-data.api.ts
+++ libs/backoffice/widgets/service-creation-widget/src/services/post-confirm-data.api.ts
@@ -1,32 +1,34 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { updateMessageAction, useAppDispatch, useAppState } from '../context';
+import { addStepErrors, ErrorPayload, StepNames, updateMessageAction, useAppDispatch, useAppState } from '../context';
 import Api from './api';
 import { ApiUtil, RQKEYS } from '@oxygen/utils';
+import { AxiosError, isAxiosError } from 'axios';
 
-// const errorsMap = {
-//   'service.name': { stepName: 'generalInfo', inputName: 'englishName' },
-//   'service.throughput': { stepName: 'generalInfo', inputName: 'throughput' },
-//   'service.version': { stepName: 'generalInfo', inputName: 'version' },
-//   'service.owner': { stepName: 'generalInfo', inputName: 'owner' },
-//   'route.method': { stepName: 'route', inputName: 'actionOrMethod' },
-//   'route.protocol': { stepName: 'route', inputName: 'protocol' },
-//   'route.path': { stepName: 'route', inputName: 'path' },
-//   'route.host': { stepName: 'route', inputName: 'host' },
-// } satisfies Record<string, { stepName: StepNames; inputName: string }>;
+const errorsMap = {
+  'service.name': { stepName: 'generalInfo', inputName: 'englishName' },
+  'service.throughput': { stepName: 'generalInfo', inputName: 'throughput' },
+  'service.version': { stepName: 'generalInfo', inputName: 'version' },
+  'service.owner': { stepName: 'generalInfo', inputName: 'owner' },
+  'route.method': { stepName: 'route', inputName: 'actionOrMethod' },
+  'route.protocol': { stepName: 'route', inputName: 'protocol' },
+  'route.path': { stepName: 'route', inputName: 'path' },
+  'route.host': { stepName: 'route', inputName: 'host' },
+} satisfies Record<string, { stepName: StepNames; inputName: string }>;
 
-// type Keys = keyof typeof errorsMap;
+type Keys = keyof typeof errorsMap;
 
-// type PublisherErrorResponse = {
-//   responseCode: string;
-//   message: string;
-//   detail: string;
-//   LocalDateTime: string;
-//   errors: Partial<Record<Keys, string>>;
-// };
+type PublisherErrorResponse = {
+  responseCode: string;
+  message: string;
+  detail: string;
+  LocalDateTime: string;
+  errors: Partial<Record<Keys, string>>;
+};
 
-// function isPublisherError(err: AxiosError): err is AxiosError<PublisherErrorResponse> {
-//   return 'errors' in err && typeof err.errors === 'object';
-// }
+function isPublisherError(err: AxiosError<unknown>): err is AxiosError<PublisherErrorResponse> {
+  const errorData = err?.response?.data;
+  return !!errorData && typeof errorData === 'object' && 'errors' in errorData && typeof errorData.errors === 'object';
+}
 
 export const usePostConfirmData = () => {
   const dispatch = useAppDispatch();
@@ -40,22 +42,22 @@ export const usePostConfirmData = () => {
       await queryClient.invalidateQueries({ queryKey: [RQKEYS.SERVICES_LIST.DRAFTS], refetchType: 'none' });
     },
     onError: (e) => {
-      const err = ApiUtil.getErrorMessage(e);
-      updateMessageAction(dispatch, err);
-      // if (isAxiosError(e) && isPublisherError(e)) {
-      //   const errorsObj = e?.response?.data?.errors;
-      //   if (errorsObj) {
-      //     const stepErrors = Object.entries(errorsObj).reduce(
-      //       (acc, [key, value]) => ({
-      //         ...acc,
-      //         [errorsMap[key].stepName]: { ...acc[errorsMap[key].stepName], [errorsMap[key].inputName]: value },
-      //       }),
-      //       {} as ErrorPayload
-      //     );
-      //
-      //     addStepErrors(dispatch, stepErrors);
-      //   }
-      // }
+      if (isAxiosError(e) && isPublisherError(e)) {
+        const errorsObj = e?.response?.data?.errors;
+        if (errorsObj) {
+          const stepErrors = Object.entries(errorsObj).reduce(
+            (acc, [key, value]) => ({
+              ...acc,
+              [errorsMap[key].stepName]: { ...acc[errorsMap[key].stepName], [errorsMap[key].inputName]: value },
+            }),
+            {} as ErrorPayload
+          );
+          addStepErrors(dispatch, stepErrors);
+        }
+      } else {
+        const err = ApiUtil.getErrorMessage(e);
+        updateMessageAction(dispatch, err);
+      }
     },
   });
 };
