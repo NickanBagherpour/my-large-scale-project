155b8c6c7becb25bc130d1d0dc15f37e4032ce8e
e.tajik <e.tajik@sadad.co.ir>
2025-01-11 12:29:30 +0330

    feat: create widget

---
+++ apps/customer-portal/src/app/(dashboard)/data-analytics/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import DataAnalyticsWidget from '@oxygen/customer/widgets/data-analytics-widget';
+
+export default function Index(props) {
+  return <DataAnalyticsWidget parentProps={props} />;
+}
---
+++ libs/customer/widgets/data-analytics-widget/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'data-analytics-widget',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/widgets/data-analytics-widget',
+};
