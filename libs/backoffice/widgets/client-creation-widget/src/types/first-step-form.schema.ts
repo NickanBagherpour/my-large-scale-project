ea55ae8b2f4db12e00300051387436d8d9aef0e7
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-02-03 13:25:21 +0330

    feat: update the validation base on the global validation

--- libs/backoffice/widgets/client-creation-widget/src/types/first-step-form.schema.ts
+++ libs/backoffice/widgets/client-creation-widget/src/types/first-step-form.schema.ts
@@ -1,155 +1,20 @@
 import { z } from 'zod';
 import { FORM_ITEM } from '../utils/consts';
+import { TFunction } from 'i18next';
+import { createValidationSchema } from '@oxygen/utils';
 
-export const createFormSchema = (t: (key: string) => string) => {
-  const requiredString = z.string({ required_error: t('validation.required') }).superRefine((value, ctx) => {
-    if (value.trim().length === 0) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: t('validation.required'),
-      });
-    }
-    if (value.length < 1) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.too_small,
-        type: 'string',
-        minimum: 1,
-        inclusive: true,
-        message: t('validation.required'),
-      });
-    }
-  });
-
-  const urlString = z.string({ required_error: t('validation.required') }).superRefine((value, ctx) => {
-    // Check if the value is empty or only whitespace
-    if (value.trim().length === 0) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: t('validation.required'),
-      });
-      return;
-    }
-
-    // Validate length
-    if (value.length < 1) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.too_small,
-        type: 'string',
-        minimum: 1,
-        inclusive: true,
-        message: t('validation.required'),
-      });
-      return;
-    }
-
-    try {
-      new URL(value);
-    } catch {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: t('error.invalid_url'),
-      });
-    }
-  });
-
-  const optionalUrlString = z
-    .string()
-    .optional()
-    .superRefine((value, ctx) => {
-      if (value && value.trim().length > 0) {
-        try {
-          new URL(value);
-        } catch {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: t('error.invalid_url'),
-          });
-        }
-      }
-    });
-  const optionalEmailString = z
-    .string()
-    .optional()
-    .superRefine((value, ctx) => {
-      if (value && value.trim().length > 0) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(value)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: t('error.invalid_email'),
-          });
-        }
-      }
-    });
-
-  const mobileNumber = z
-    .string({ required_error: t('validation.required') })
-    .trim()
-    .superRefine((value, ctx) => {
-      if (value.length < 1) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.too_small,
-          type: 'string',
-          minimum: 1,
-          inclusive: true,
-          message: t('validation.required'),
-        });
-        return;
-      }
-
-      if (value.length < 11) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.too_small,
-          type: 'string',
-          minimum: 11,
-          inclusive: true,
-          message: t('error.min_length'),
-        });
-      }
-    });
-
-  // .min(11, { message: t('error.min_length') });
+export const createFormSchema = (t: TFunction) => {
+  const validationSchema = createValidationSchema(t);
 
   return z.object({
-    // Name Fields
-    [FORM_ITEM.latin_name_client]: requiredString.superRefine((value, ctx) => {
-      if (value.trim() !== '' && !/^[A-Za-z\s]+$/.test(value)) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: t('error.english_only'),
-        });
-      }
-    }),
-
-    [FORM_ITEM.persian_name_client]: requiredString.superRefine((value, ctx) => {
-      if (value.trim() !== '' && !/^[\u0600-\u06FF\s]+$/.test(value)) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: t('error.persian_only'),
-        });
-      }
-    }),
-
-    // Required Fields
-    [FORM_ITEM.client_type]: requiredString,
-    [FORM_ITEM.user_name]: requiredString,
-    [FORM_ITEM.national_code]: requiredString,
-    [FORM_ITEM.organization_name]: requiredString,
-    [FORM_ITEM.client_id]: requiredString,
-    [FORM_ITEM.identity_auth]: requiredString,
-
-    //mobile number
-    [FORM_ITEM.mobile_number]: mobileNumber,
-
-    // optional URL Field
-    [FORM_ITEM.input_address]: optionalUrlString,
-    [FORM_ITEM.website_url]: optionalUrlString,
-
-    //optional email validator
-    [FORM_ITEM.email]: optionalEmailString,
-
-    // URL Field
-    [FORM_ITEM.return_address]: urlString,
+    [FORM_ITEM.CLIENT_ENGLISH_NAME]: validationSchema.english,
+    [FORM_ITEM.CLIENT_PERSIAN_NAME]: validationSchema.persian,
+    [FORM_ITEM.CLIENT_KEY]: validationSchema.required,
+    [FORM_ITEM.CLIENT_TYPE_CODE]: validationSchema.idSelection,
+    [FORM_ITEM.AUTHORIZATION_KEY]: validationSchema.required,
+    [FORM_ITEM.WEBSITE_URL]: validationSchema.url,
+    [FORM_ITEM.INBOUND_ADDRESS]: validationSchema.url,
+    [FORM_ITEM.REDIRECT_URL]: validationSchema.url,
   });
 };
 
