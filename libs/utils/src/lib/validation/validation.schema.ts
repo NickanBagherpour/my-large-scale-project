1bf7a4958efbbfabe15cb2aae523bd78247dd5ba
m.jafari <M.Jafari@sadad.co.ir>
2025-02-05 14:13:42 +0330

    fix: use validationSchema for form validation in customer

--- libs/utils/src/lib/validation/validation.schema.ts
+++ libs/utils/src/lib/validation/validation.schema.ts
@@ -3,7 +3,12 @@ import { TFunction } from 'i18next';
 import { REGEX_PATTERNS } from '../pattern-util';
 import { limits } from './constants';
 
-export const createValidationSchema = (t: TFunction) => {
+export const createValidationSchema = (
+  t: TFunction,
+  organizationNationalIdNumber?: number,
+  organizationEconomyCodedNumber?: number,
+  postalCodedNumber?: number
+) => {
   const validationSchema = {
     required: z
       .string({
@@ -12,6 +17,15 @@ export const createValidationSchema = (t: TFunction) => {
       })
       .trim()
       .min(limits.DEFAULT_MIN_LENGTH, { message: t('validation.required') })
+      .max(limits.DEFAULT_MAX_LENGTH, { message: t('validation.max_length') }),
+
+    simpleRequired: z
+      .string({
+        required_error: t('validation.required'),
+        invalid_type_error: t('validation.required') /* if value is null */,
+      })
+      .trim()
+      .min(1, { message: t('validation.required') })
       .max(limits.DEFAULT_MAX_LENGTH, { message: t('validation.max_length') }),
 
     idSelection: z.number({ required_error: t('validation.choose_one_option') }),
@@ -43,6 +57,45 @@ export const createValidationSchema = (t: TFunction) => {
       .trim()
       .min(limits.ORGANIZATION_NUMBER, { message: t('validation.required') })
       .max(limits.ORGANIZATION_NUMBER, {
+        message: t('validation.max_length'),
+      })
+      .regex(REGEX_PATTERNS.onlyDigit, {
+        message: t('validation.english_name_error'),
+      }),
+
+    organizationNationalIdNumber: z
+      .string({ required_error: t('validation.required'), invalid_type_error: t('validation.required') })
+      .trim()
+      .min(limits.ORGANIZATION_NATIONAL_ID_NUMBER, {
+        message: t('validation.min_len', { val: organizationNationalIdNumber }),
+      })
+      .max(limits.ORGANIZATION_NATIONAL_ID_NUMBER, {
+        message: t('validation.max_length'),
+      })
+      .regex(REGEX_PATTERNS.onlyDigit, {
+        message: t('validation.english_name_error'),
+      }),
+
+    organizationEconomyCodeNumber: z
+      .string({ required_error: t('validation.required'), invalid_type_error: t('validation.required') })
+      .trim()
+      .min(limits.ORGANIZATION_ECONOMY_CODE_NUMBER, {
+        message: t('validation.min_len', { val: organizationEconomyCodedNumber }),
+      })
+      .max(limits.ORGANIZATION_ECONOMY_CODE_NUMBER, {
+        message: t('validation.max_length'),
+      })
+      .regex(REGEX_PATTERNS.onlyDigit, {
+        message: t('validation.english_name_error'),
+      }),
+
+    postalCode: z
+      .string({ required_error: t('validation.required'), invalid_type_error: t('validation.required') })
+      .trim()
+      .min(limits.POSTAL_CODE_NUMBER, {
+        message: t('validation.min_len', { val: postalCodedNumber }),
+      })
+      .max(limits.POSTAL_CODE_NUMBER, {
         message: t('validation.max_length'),
       })
       .regex(REGEX_PATTERNS.onlyDigit, {
