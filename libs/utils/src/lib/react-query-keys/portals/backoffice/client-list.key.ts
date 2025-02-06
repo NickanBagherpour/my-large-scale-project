a289a6c755b33542bdd03c75c2032e700c0da907
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-02-06 17:53:09 +0330

    fix: resolve type errors

--- libs/utils/src/lib/react-query-keys/portals/backoffice/client-list.key.ts
+++ libs/utils/src/lib/react-query-keys/portals/backoffice/client-list.key.ts
@@ -1,4 +1,10 @@
 import { createQueryKeys } from '../../create-query-keys';
 
 const prefix = 'CLIENTS-LIST';
-export const KEYS = createQueryKeys(prefix, ['CLIENTS', 'DRAFTS', 'CLIENT_PLUGINS', 'CLIENT_SERVICE_PLUGINS']);
+export const KEYS = createQueryKeys(prefix, [
+  'CLIENTS',
+  'DRAFTS',
+  'CLIENT_PLUGINS',
+  'CLIENT_SERVICE_PLUGINS',
+  'INQUIRY',
+]);
