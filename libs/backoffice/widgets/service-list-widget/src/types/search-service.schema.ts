f17ff3e187cef56e5d9efb634a7cec170f87eec7
e.tajik <e.tajik@sadad.co.ir>
2025-02-07 15:43:21 +0330

    refactor: integrate service fields validation

--- libs/backoffice/widgets/service-list-widget/src/types/search-service.schema.ts
+++ libs/backoffice/widgets/service-list-widget/src/types/search-service.schema.ts
@@ -1,16 +1,12 @@
 import { TFunction } from 'i18next';
 import { z } from 'zod';
 import { SERVICE_NAME } from '../utils/consts';
-import { REGEX_PATTERNS } from '@oxygen/utils';
+import { createValidationSchema, REGEX_PATTERNS } from '@oxygen/utils';
 
-export const CreateServiceNameSchema = (t: TFunction<'translation', undefined>) =>
-  z.object({
-    [SERVICE_NAME.ServiceName]: z
-      .string()
-      .trim()
-      .max(100, t('validation.min_len', { val: 100 }))
-      .regex(REGEX_PATTERNS.isLatinText, t('validation.english_validation_message'))
-      .nullable()
-      .optional(),
+export const CreateServiceNameSchema = (t: TFunction<'translation', undefined>) => {
+  const validationSchema = createValidationSchema(t);
+  return z.object({
+    [SERVICE_NAME.ServiceName]: validationSchema.searchField,
   });
+};
 export type ServiceNameType = z.infer<ReturnType<typeof CreateServiceNameSchema>>;
