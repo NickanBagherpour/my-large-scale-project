a998a6df487d6c9e3a3f58470c387010a18db3e8
S.Salehpour <s.salehpour@sadad.co.ir>
2025-02-07 10:01:15 +0330

    refactor: add upstream validation

--- libs/reusable-components/src/components/add-upstream-modal/add-upstream-modal.schema.ts
+++ libs/reusable-components/src/components/add-upstream-modal/add-upstream-modal.schema.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 import { TFunction } from 'i18next';
 
-import { createValidationSchema } from '@oxygen/utils';
+import { createValidationSchema, REGEX_PATTERNS } from '@oxygen/utils';
 
 export const FORM_ITEM_NAMES = {
   name: 'name',
@@ -11,8 +11,14 @@ export const FORM_ITEM_NAMES = {
 export const createUpstreamType = (t: TFunction) => {
   const validationSchema = createValidationSchema(t);
   return z.object({
-    [FORM_ITEM_NAMES.name]: validationSchema.defaultEnglishName,
-    [FORM_ITEM_NAMES.description]: validationSchema.description,
+    [FORM_ITEM_NAMES.name]: validationSchema.defaultEnglishName.regex(
+      REGEX_PATTERNS.defaultEnglishName,
+      t('validation.is_not_valid', { val: t('uikit.upstream_english_name') })
+    ),
+    [FORM_ITEM_NAMES.description]: validationSchema.defaultPersianName.regex(
+      REGEX_PATTERNS.defaultPersianName,
+      t('validation.is_not_valid', { val: t('uikit.upstream_persian_name') })
+    ),
   });
 };
 export type CreateUpstreamType = z.infer<ReturnType<typeof createUpstreamType>>;
