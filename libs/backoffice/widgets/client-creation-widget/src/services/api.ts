21d148c67eefad974482761a141625e37a82457d
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-02-15 12:50:34 +0330

    feat: clear the mock api and queries

--- libs/backoffice/widgets/client-creation-widget/src/services/api.ts
+++ libs/backoffice/widgets/client-creation-widget/src/services/api.ts
@@ -1,14 +1,10 @@
 import { client, portalUrl } from '@oxygen/client';
-import Mockify from '@oxygen/mockify';
 import { FirstStepType } from '../context/types';
 import { ClientDraftDataType } from '../types/first-step/client-draft.type';
 import { InquiryStatusResponceType } from '../types/get-client-inquiry-status.type';
 import { OrganizationInfoResponceType } from '../types';
 
 const Api = {
-  // getTableReportData: async (params: FetchParamsType) => Mockify.clientCreationTable(params),
-  getMainCardData: async () => Mockify.getPlugins(),
-  getSelectData: async () => Mockify.getSelectOptions(),
   postClient: async (params) => client.post<FirstStepType>(`${portalUrl}/v1/clients`, { ...params }),
   putProgress: async ({ clientName, progressCode }) =>
     client.put(`${portalUrl}/v1/clients/${clientName}/progress/${progressCode}`),
