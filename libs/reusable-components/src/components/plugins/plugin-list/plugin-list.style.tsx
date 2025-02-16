1cf962da0b51cd4a467aef3376fdec1ead5d61be
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-02-16 12:51:17 +0330

    fix: show scopes in tag

--- libs/reusable-components/src/components/plugins/plugin-list/plugin-list.style.tsx
+++ libs/reusable-components/src/components/plugins/plugin-list/plugin-list.style.tsx
@@ -1,7 +1,11 @@
+import { respondTo } from '@oxygen/utils';
 import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
   align-items: center;
   gap: 1.6rem;
+  ${respondTo.down('sm')} {
+    flex-direction: column;
+  }
 `;
