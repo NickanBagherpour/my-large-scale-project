01b19ef33066217ed06a6c925fa8c6b257f9b9a2
S.Salehpour <s.salehpour@sadad.co.ir>
2025-02-22 08:37:53 +0330

    fix: reported bug 45

--- libs/backoffice/widgets/upstream-list-widget/src/components/confirm-delete-modal/confirm-delete-modal.tsx
+++ libs/backoffice/widgets/upstream-list-widget/src/components/confirm-delete-modal/confirm-delete-modal.tsx
@@ -6,25 +6,23 @@ import { Nullable } from '@oxygen/types';
 import { Loading } from '@oxygen/ui-kit';
 
 import { updateMessageAction, useAppDispatch } from '../../context';
-import { GetUpstreamServiceResponseType } from '../../types';
-import { useDeleteUpstream } from '../../services';
+import { useDeleteUpstream, useGetUpstreamServicesQuery } from '../../services';
 
 import * as S from './confirm-delete-modal.style';
 
 type Props = {
   openModal: boolean;
   setOpenModal: (value: ((prevState: boolean) => boolean) | boolean) => void;
-  services: GetUpstreamServiceResponseType;
   upstreamName: Nullable<string>;
-  isFetching: boolean;
 };
 const ConfirmDeleteModal: React.FC<Props> = (props) => {
-  const { openModal, setOpenModal, services, upstreamName, isFetching } = props;
+  const { openModal, setOpenModal, upstreamName } = props;
   const [t] = useTr();
   const theme = useAppTheme();
   const dispatch = useAppDispatch();
 
   const { mutate, isPending } = useDeleteUpstream();
+  const { data: services, isFetching } = useGetUpstreamServicesQuery(upstreamName);
   const handleDeleteUpstream = async (params) => {
     await mutate(params, {
       onSuccess: () => {
