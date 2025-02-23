809b610c7085e5cae9562dae36851d1636f84004
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-02-23 14:49:10 +0330

    feat: work on top section

--- libs/business/widgets/tariff-list-widget/src/components/top-section/top-section.style.tsx
+++ libs/business/widgets/tariff-list-widget/src/components/top-section/top-section.style.tsx
@@ -1,3 +1,43 @@
+import { respondTo } from '@oxygen/utils';
+import { Input as KitInput, Button as KitButton } from '@oxygen/ui-kit';
+
 import styled from 'styled-components';
 
-export const TopSectionContainer = styled.div``;
+export const TopSectionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1.2rem;
+  margin: 2.4rem 0;
+
+  ${respondTo.down('sm')} {
+    flex-direction: column;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+
+  ${respondTo.down('sm')} {
+    width: 100%;
+    justify-content: space-between;
+  }
+`;
+
+export const Button = styled(KitButton)`
+  ${respondTo.down('sm')} {
+    width: 100%;
+  }
+`;
+
+export const Input = styled(KitInput)`
+  max-width: 50.4rem;
+  margin-inline-end: auto;
+
+  ${respondTo.down('lg')} {
+    max-width: 100%;
+    font-size: 1.7rem;
+  }
+`;
--- libs/business/widgets/tariff-list-widget/src/components/top-section/top-section.tsx
+++ libs/business/widgets/tariff-list-widget/src/components/top-section/top-section.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './top-section.style';
 import { PageProps } from '@oxygen/types';
+import { useBounce, useToggle } from '@oxygen/hooks';
+import { useAppDispatch } from '../../context';
+import { useTr } from '@oxygen/translation';
+import { InquiryComponent } from '@oxygen/reusable-components';
 type TopSectionPropsType = PageProps & {
   //
 };
 export const TopSection: React.FC<TopSectionPropsType> = (props) => {
-  return <S.TopSectionContainer>top-section</S.TopSectionContainer>;
+  const dispatch = useAppDispatch();
+  const [t] = useTr();
+
+  const [value, setValue] = useState('');
+  const [isInquiryModalOpen, toggleInquiryModal] = useToggle(false);
+
+  useBounce(() => {
+    console.log(value.trim());
+  }, [value.trim()]);
+
+  return (
+    <S.TopSectionContainer>
+      <S.Input
+        value={value}
+        placeholder={t('search_placeholder')}
+        prefix={<i className='icon-search-normal' />}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <S.ButtonContainer>
+        <S.Button onClick={toggleInquiryModal} color='primary' variant='solid'>
+          {t('add_tariff')}
+        </S.Button>
+      </S.ButtonContainer>
+      {isInquiryModalOpen && <InquiryComponent type='client' dispatch={dispatch} toggle={toggleInquiryModal} />}
+    </S.TopSectionContainer>
+  );
 };
