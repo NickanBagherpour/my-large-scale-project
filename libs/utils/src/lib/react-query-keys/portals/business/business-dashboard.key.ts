ed369b9c2a72849f8113ec0c7fb103d9941b5e6e
e.tajik <e.tajik@sadad.co.ir>
2025-03-02 15:41:23 +0330

    feat: connect dashboard card to api

--- libs/utils/src/lib/react-query-keys/portals/business/business-dashboard.key.ts
+++ libs/utils/src/lib/react-query-keys/portals/business/business-dashboard.key.ts
@@ -1,4 +1,4 @@
 import { createQueryKeys } from '../../create-query-keys';
 
 const prefix = 'BUSINESS_DASHBOARD';
-export const KEYS = createQueryKeys(prefix, ['GET_LIST', 'SEARCH']);
+export const KEYS = createQueryKeys(prefix, ['GET_LIST', 'SEARCH', 'CARDS']);
