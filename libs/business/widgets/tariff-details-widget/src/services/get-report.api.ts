3ad6a09059f5b3174132cb2fc5a2b439c9ae4bd3
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-03-05 10:46:54 +0330

    feat: connect api

--- libs/business/widgets/tariff-details-widget/src/services/get-report.api.ts
+++ libs/business/widgets/tariff-details-widget/src/services/get-report.api.ts
@@ -4,12 +4,12 @@ import { RQKEYS, withErrorHandling } from '@oxygen/utils';
 import { useAppDispatch } from '../context';
 import Api from './api';
 
-export const useGetReportDataQuery = (params: string) => {
+export const useGetReportDataQuery = (serviceName: string) => {
   const dispatch = useAppDispatch();
 
   return useQuery({
-    queryKey: [RQKEYS.BUSINESS.TARIFF_DETAILS.GET_LIST, params],
-    queryFn: withErrorHandling(() => Api.getReportData(params), dispatch),
+    queryKey: [RQKEYS.BUSINESS.TARIFF_DETAILS.GET_LIST, serviceName],
+    queryFn: withErrorHandling(() => Api.getReportData(serviceName), dispatch),
     networkMode: 'offlineFirst',
   });
 };
