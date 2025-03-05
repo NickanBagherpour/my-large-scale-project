e747f763ff14fc0cfbf33742385efede9bc71270
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-03-05 09:29:41 +0330

    feat: work on api and api type in tariff details

--- libs/business/widgets/tariff-details-widget/src/services/get-report.api.ts
+++ libs/business/widgets/tariff-details-widget/src/services/get-report.api.ts
@@ -1,15 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { RQKEYS, withErrorHandling } from '@oxygen/utils';
-import { FetchParamsType } from '../types';
 import { useAppDispatch } from '../context';
 import Api from './api';
 
-export const useGetReportDataQuery = (params: FetchParamsType) => {
+export const useGetReportDataQuery = (params: string) => {
   const dispatch = useAppDispatch();
 
   return useQuery({
     queryKey: [RQKEYS.BUSINESS.TARIFF_DETAILS.GET_LIST, params],
     queryFn: withErrorHandling(() => Api.getReportData(params), dispatch),
+    networkMode: 'offlineFirst',
   });
 };
