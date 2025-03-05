b8ce640b275cf2331b1b9ae41431ff054f00ec90
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-03-05 09:04:21 +0330

    feat: add correct api for delete in tariff list

--- libs/business/widgets/tariff-list-widget/src/services/api.ts
+++ libs/business/widgets/tariff-list-widget/src/services/api.ts
@@ -1,11 +1,11 @@
-import { client, invoiceUrl, portalUrl } from '@oxygen/client';
+import { client, invoiceUrl } from '@oxygen/client';
 //TODO: set type of response and params and the url after api is be ready
 const Api = {
   getTariffListData: async (params) => {
     return client.get<any>(`${invoiceUrl}/v1/service-fees`, { params });
   },
   deleteService: async (serviceName: string) => {
-    return client.delete<unknown>(`${portalUrl}/v1/clients/${serviceName}`);
+    return client.delete<unknown>(`${invoiceUrl}/v1/service-fees/service-name/${serviceName}`);
   },
 };
 export default Api;
