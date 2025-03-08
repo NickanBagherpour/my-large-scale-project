01f21bdd8dd5b5b442d545273cd938cf6c47c341
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-03-08 15:37:31 +0330

    fix: decrease the space between footer and the tariff component

--- libs/business/widgets/upsert-tarrif-widget/src/components/app/app.style.tsx
+++ libs/business/widgets/upsert-tarrif-widget/src/components/app/app.style.tsx
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import { Footer as AppFooter } from '@oxygen/reusable-components';
 
 export const Footer = styled(AppFooter)`
-  margin-top: 12rem;
+  margin-top: 4rem;
 `;
