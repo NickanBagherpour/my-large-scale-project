3f135d01a664c14ed3a4b5baa3dfd6149e27c8f9
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-03-08 16:34:53 +0330

    fix: correct file name

--- libs/business/widgets/services-report-widget/src/services/index.ts
+++ libs/business/widgets/services-report-widget/src/services/index.ts
@@ -1,3 +1,3 @@
 export { default as Api } from './api';
 export { useGetServicesReportQuery } from './get-services-report.api';
-export { useGetServiceClients } from './get-service-Clients.api';
+export { useGetServiceClients } from './get-service-clients.api';
