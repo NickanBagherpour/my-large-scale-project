1c235956b24ad1080d8736f03b3799aaf700c769
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-12 15:51:00 +0330

    feat: add constant variable

--- libs/business/widgets/service-management-widget/src/utils/consts.ts
+++ libs/business/widgets/service-management-widget/src/utils/consts.ts
@@ -1,11 +1,11 @@
 export const AVAILABLE_ROWS_PER_PAGE = [5, 10, 25, 50, 100];
-export const INITIAL_ROW_PER_PAGE = AVAILABLE_ROWS_PER_PAGE[0];
-export const INITIAL_PAGE = 1;
+export const INITIAL_ROW_PER_PAGE = AVAILABLE_ROWS_PER_PAGE[1];
+export const INITIAL_PAGE = 0;
 export const ALL_STATUS_LIST = null;
 
 export enum SORT_ORDER {
-  ASCENDING = 'desc',
-  DESCENDING = 'createDate',
+  ASCENDING = 'asc',
+  DESCENDING = 'desc',
 }
 
 // status-enums
--- libs/business/widgets/service-management-widget/src/utils/table-list.tsx
+++ libs/business/widgets/service-management-widget/src/utils/table-list.tsx
@@ -1,29 +1,32 @@
 import { Tooltip } from '@oxygen/ui-kit';
 import { CONSTANTS, getValueOrDash } from '@oxygen/utils';
-
+import * as S from '../components/table-container/table-container.style';
 export type TableColumnsPropsType = {
   t: any;
   data?: any[];
   pagination: any;
 };
-
 export const getDesktopColumns = (props: TableColumnsPropsType) => {
   const { t, pagination } = props;
-  const { page, rowsPerPage } = pagination;
+  const statusOptions = [
+    { value: 'true', label: t('chips.commercial') },
+    { value: 'false', label: t('chips.noncommercial') },
+  ];
+  const { page, limit } = pagination;
   return [
     {
       title: t('common.row_number'),
       key: 'index',
       width: CONSTANTS.ROW_INDEX_WIDTH,
       render: (_val, _record, index) => {
-        const start = (page - 1) * rowsPerPage + 1;
+        const start = page * limit + 1;
         return start + index;
       },
     },
     {
       title: t('field.service_fa_name'),
-      dataIndex: 'name',
-      key: 'name',
+      dataIndex: 'key',
+      key: 'key',
       render: (value) => (
         <Tooltip placement='top' title={getValueOrDash(value)}>
           {`${getValueOrDash(value)}`}
@@ -32,8 +35,8 @@ export const getDesktopColumns = (props: TableColumnsPropsType) => {
     },
     {
       title: t('field.service_en_name'),
-      dataIndex: 'name',
-      key: 'name',
+      dataIndex: 'value',
+      key: 'value',
       render: (value) => (
         <Tooltip placement='top' title={getValueOrDash(value)}>
           {`${getValueOrDash(value)}`}
@@ -44,7 +47,19 @@ export const getDesktopColumns = (props: TableColumnsPropsType) => {
       title: t('commercialization_status'),
       width: '20rem',
       ellipsis: false,
-      render: () => <a>Delete</a>,
+      render: (_val, _record) => {
+        return (
+          <S.CustomeSelect
+            options={statusOptions}
+            defaultValue={_val.isComertial ? statusOptions[0] : statusOptions[1]}
+            size='middle'
+            onChange={() => {
+              console.log('clicked');
+            }}
+            $isComertial={_val.isComertial}
+          />
+        );
+      },
     },
   ];
 };
