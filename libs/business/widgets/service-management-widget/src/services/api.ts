43be1ccf57ac119c8a472dcb54c442383608efa7
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-13 16:51:07 +0330

    feat: add the all api to the api file

--- libs/business/widgets/service-management-widget/src/services/api.ts
+++ libs/business/widgets/service-management-widget/src/services/api.ts
@@ -6,5 +6,11 @@ const Api = {
   getTableReportData: async (params: getTableReportParamsType) => {
     return client.get<TableResponseType>(`${portalUrl}/management/api/v1/services`, { params });
   },
+  getServiceClients: async (serviceName: string) => {
+    return client.get<string[]>(`${portalUrl}/management/api/v1/services/${serviceName}/organizations`);
+  },
+  putClientCommercialStatus: async (serviceName: string) => {
+    return client.put(`${portalUrl}/management/api/v1/services/${serviceName}`);
+  },
 };
 export default Api;
