4ff28b4ccc9b66a98ceb975c7793192b36f8e93d
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-04-14 11:40:31 +0330

    refactor: remove ununsed things

--- libs/business/widgets/clients-detailed-bill-widget/src/context/actions.ts
+++ libs/business/widgets/clients-detailed-bill-widget/src/context/actions.ts
@@ -1,19 +1,5 @@
-import { ApiUtil } from '@oxygen/utils';
 import { MessageType, Nullable } from '@oxygen/types';
-
-import { FiltersType, PaginationType, WidgetActionType, WidgetDispatchType } from './types';
-
-export function updateFiltersAction(dispatch: WidgetDispatchType, filters: Partial<FiltersType>) {
-  dispatch({ type: 'UPDATE_FILTERS', payload: filters });
-}
-
-export function updateSubmitAction(dispatch: WidgetDispatchType, submit: Partial<FiltersType>) {
-  dispatch({ type: 'UPDATE_SUBMIT', payload: submit });
-}
-
-export function updatePagination(dispatch: WidgetDispatchType, pagination: Partial<PaginationType>) {
-  dispatch({ type: 'UPDATE_PAGINATION', payload: pagination });
-}
+import { WidgetDispatchType } from './types';
 
 export function updateMessageAction(dispatch: WidgetDispatchType, message: Nullable<MessageType>) {
   dispatch({ type: 'UPDATE_GLOBAL_MESSAGE', payload: message });
@@ -21,10 +7,4 @@ export function updateMessageAction(dispatch: WidgetDispatchType, message: Nulla
 
 export function resetErrorMessageAction(dispatch: WidgetDispatchType) {
   updateMessageAction(dispatch, null);
-}
-
-function handleError(dispatch, actionType: WidgetActionType['type'], reason, extraPayload) {
-  const message = ApiUtil.getErrorMessage(reason);
-  dispatch({ type: actionType, payload: { message, ...extraPayload } });
-  return null;
 }
