64d464529ebae714c1bfa9b6a5cf309973d53f5a
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-14 17:13:55 +0330

    feat: generate the widget

---
+++ apps/backoffice-portal/src/app/(dashboard)/organization-management/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import OrganizationManagementWidget from '@oxygen/backoffice/widgets/organization-management-widget';
+
+export default function Index(props) {
+  return <OrganizationManagementWidget parentProps={props} />;
+}
---
+++ libs/backoffice/widgets/organization-management-widget/src/locales/en.ts
@@ -0,0 +1,3 @@
+export default {
+  widget_name: 'organization-management-widget',
+};
