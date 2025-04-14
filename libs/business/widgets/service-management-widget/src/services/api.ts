69ca33e2695823d08e68672533385a2122167a64
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-14 14:54:50 +0330

    feat: add api prefix to the api list

--- libs/business/widgets/service-management-widget/src/services/api.ts
+++ libs/business/widgets/service-management-widget/src/services/api.ts
@@ -1,16 +1,16 @@
-import { client, portalUrl } from '@oxygen/client';
+import { API_PREFIX, client } from '@oxygen/client';
 
 import { getTableReportParamsType, TableResponseType } from '../types';
 
 const Api = {
   getTableReportData: async (params: getTableReportParamsType) => {
-    return client.get<TableResponseType>(`${portalUrl}/management/api/v1/services`, { params });
+    return client.get<TableResponseType>(`${API_PREFIX.MANAGEMENT}/v1/services`, { params });
   },
   getServiceClients: async (serviceName: string) => {
-    return client.get<string[]>(`${portalUrl}/management/api/v1/services/${serviceName}/organizations`);
+    return client.get<string[]>(`${API_PREFIX.MANAGEMENT}/v1/services/${serviceName}/organizations`);
   },
   putServiceCommercialStatus: async (serviceName: string) => {
-    return client.put(`${portalUrl}/management/api/v1/services/${serviceName}`);
+    return client.put(`${API_PREFIX.MANAGEMENT}/v1/services/${serviceName}`);
   },
 };
 export default Api;
