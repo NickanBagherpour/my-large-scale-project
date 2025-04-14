f34d4beb49695356eb1bde9d78e33eb4df3d30b9
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-14 11:21:33 +0330

    feat: handle the modal message and the notif message

--- libs/business/widgets/service-management-widget/src/components/table-container/table-container.tsx
+++ libs/business/widgets/service-management-widget/src/components/table-container/table-container.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { TablePaginationConfig } from 'antd';
+
+import { useApp } from '@oxygen/hooks';
 import { Nullable } from '@oxygen/types';
 import { useTr } from '@oxygen/translation';
-import { Button, Modal } from '@oxygen/ui-kit';
+import { Button, MarkText, Modal } from '@oxygen/ui-kit';
 
+import { useGetServiceClientsQuery, usePutServiceCommercialStatusMutation } from '../../services';
+import { updatePagination, useAppDispatch, useAppState } from '../../context';
+import { TableRowValueType } from '../../types/table-row-value.type';
+import { AVAILABLE_ROWS_PER_PAGE } from '../../utils/consts';
 import { getDesktopColumns } from '../../utils/table-list';
 import { TableResponseType } from '../../types';
-import { updatePagination, useAppDispatch, useAppState } from '../../context';
-
-import { TablePaginationConfig } from 'antd';
-import { AVAILABLE_ROWS_PER_PAGE } from '../../utils/consts';
 
 import * as S from './table-container.style';
 
@@ -22,15 +25,45 @@ export const TableContainer = (props: TableContainerPropsType) => {
   const state = useAppState();
   const dispatch = useAppDispatch();
   const [t] = useTr();
+  const { notification } = useApp();
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const close = () => setModalIsOpen(false);
+  const [modalIsOpen, setModalIsOpen] = useState<TableRowValueType | null>(null);
+  const close = () => setModalIsOpen(null);
 
+  const { mutate, isPending } = usePutServiceCommercialStatusMutation();
+  const {
+    data: serviceClientData,
+    isFetching: serviceClientIsFetching,
+    refetch,
+  } = useGetServiceClientsQuery(modalIsOpen?.name ?? '');
+
+  useEffect(() => {
+    if (modalIsOpen?.isCommercial) {
+      refetch();
+    }
+  }, [modalIsOpen?.isCommercial]);
+
+  const listLength = serviceClientData && serviceClientData.length;
+  const serviceCount = `${listLength} سرویس`;
   const pagination = state.table.pagination;
   const tableData = data?.content ?? [];
 
   const lastValidTotal = data?.page?.totalElements;
   const [lastTotal, setLastTotal] = useState(lastValidTotal);
+
+  const handleSubmit = async () => {
+    const serviceName = modalIsOpen?.name;
+    const servicePersianName = modalIsOpen?.persianName;
+    const commercialStatus = modalIsOpen?.isCommercial ? t('noncommercial') : t('commercial');
+    mutate(serviceName!, {
+      onSuccess: async () => {
+        setModalIsOpen(null);
+        notification.success({
+          message: t('success_notif', { servicePersianName, commercialStatus }),
+        });
+      },
+    });
+  };
 
   const handlePageChange = async ({ current, pageSize }: TablePaginationConfig) => {
     if (lastValidTotal) setLastTotal(lastValidTotal); //in case one page has error still let it paginate
@@ -66,26 +99,44 @@ export const TableContainer = (props: TableContainerPropsType) => {
       <Modal
         centered
         title={t('warning')}
-        open={modalIsOpen}
-        closable={true}
+        open={!!modalIsOpen}
+        closable={!isPending}
         onCancel={close}
+        loading={serviceClientIsFetching}
         footer={[
-          <Button key={'cancel'} onClick={close} size='large' color='primary' variant='outlined' disabled={false}>
+          <Button key={'cancel'} onClick={close} size='large' color='primary' variant='outlined' disabled={isPending}>
             {t('button.cancel')}
           </Button>,
           <Button
             key={'tegister'}
-            onClick={() => console.log('you click on register button')}
+            onClick={() => handleSubmit()}
             size='large'
             color='primary'
-            loading={false}
-            disabled={false}
+            loading={isPending}
+            disabled={isPending}
           >
             {t('button.register')}
           </Button>,
         ]}
       >
-        this is the test for the modal
+        {!modalIsOpen?.isCommercial ? (
+          t('change_to_commercial_modal_text')
+        ) : listLength === 0 ? (
+          t('change_to_non_commercial_modal_text')
+        ) : (
+          <>
+            <MarkText
+              text={t('change_to_non_commercial_modal_text_with_service_count', { service_count: serviceCount })}
+              wordToHighlight={serviceCount}
+              highlightColor='success'
+            />
+            <ul>
+              {serviceClientData?.map((service, index) => (
+                <li key={index}>{service}</li>
+              ))}
+            </ul>
+          </>
+        )}
       </Modal>
     </S.TableContainer>
   );
--- libs/business/widgets/service-management-widget/src/locales/fa.ts
+++ libs/business/widgets/service-management-widget/src/locales/fa.ts
@@ -6,6 +6,13 @@ export default {
   newest: 'جدیدترین',
   commercialization_status: 'وضعیت تجاری‌سازی',
   warning: 'هشدار!',
+  change_to_commercial_modal_text: 'آیا از تغییر وضعیت سرویس به "تجاری" اطمینان دارید؟',
+  change_to_non_commercial_modal_text_with_service_count:
+    'این سرویس به {{service_count}} متصل است. آیا از تغییر وضعیت سرویس به "غیرتجاری" اطمینان دارید؟',
+  change_to_non_commercial_modal_text: 'آیا از تغییر وضعیت سرویس به "غیر تجاری" اطمینان دارید؟',
+  success_notif: 'وضعیت سرویس “{{servicePersianName}}” به {{commercialStatus}} تغییر یافت.',
+  commercial: 'تجاری',
+  noncommercial: 'غیر تجاری',
   chips: {
     all_status: 'همه وضعیت‌ها',
     commercial: 'تجاری',
