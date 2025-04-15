dd8742b10be90efc64f47785eb3a23058a0aff49
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-15 18:06:09 +0330

    feat: initial the validation

--- libs/backoffice/widgets/organization-management-widget/src/types/sample.schema.ts
+++ libs/backoffice/widgets/organization-management-widget/src/types/sample.schema.ts
@@ -1,11 +1,31 @@
 import { z } from 'zod';
+import { TFunction } from 'i18next';
+import { createValidationSchema } from '@oxygen/utils';
+import { FORM_NAME } from '../utils/consts';
 
-export const sampleSchema = z
-  .object({
-    name: z.string().max(25, 'validation.max_length').nullable(),
-    code: z.string().max(25, 'validation.max_length').nullable(),
-    branchCode: z.string().max(6, 'validation.max_length').nullable(),
-  })
-  .partial();
+export const createFormSchema = (t: TFunction) => {
+  const validationSchema = createValidationSchema(t);
 
-export type FormFieldsType = z.infer<typeof sampleSchema>;
+  return z.object({
+    [FORM_NAME.ACTIVITY_FIELD]: validationSchema.required,
+    [FORM_NAME.ECONOMY_CODE]: validationSchema.required,
+    [FORM_NAME.LAST_REGISTERED_ADDRESS]: validationSchema.required,
+    [FORM_NAME.LEGAL_ENTITY_NAME]: validationSchema.required,
+    [FORM_NAME.LEGAL_ENTITY_TYPE]: validationSchema.required,
+    [FORM_NAME.NATIONAL_ID]: validationSchema.required,
+    [FORM_NAME.REGISTRATION_DATE]: validationSchema.required,
+    [FORM_NAME.REGISTRATION_NUMBER]: validationSchema.required,
+    [FORM_NAME.TELEPHONE]: validationSchema.required,
+    [FORM_NAME.ZIP_CODE]: validationSchema.required,
+    [FORM_NAME.TECHNICAL_REPRESENTATIVE.MOBILE_NUMBER]: validationSchema.required,
+    [FORM_NAME.TECHNICAL_REPRESENTATIVE.LANDLINE_NUMBER]: validationSchema.required,
+    [FORM_NAME.TECHNICAL_REPRESENTATIVE.FIRST_AND_LAST_NAME]: validationSchema.required,
+    [FORM_NAME.REPRESENTATIVE.MOBILE_NUMBER]: validationSchema.required,
+    [FORM_NAME.REPRESENTATIVE.LANDLINE_NUMBER]: validationSchema.required,
+    [FORM_NAME.REPRESENTATIVE.FIRST_AND_LAST_NAME]: validationSchema.required,
+    //TODO : ADD VALIDATION FOR DISCRIPTION IF ITS NEEDED
+    // [FORM_ITEM.DISCRIPTION]: validationSchema.
+  });
+};
+
+export type FormValues = z.infer<ReturnType<typeof createFormSchema>>;
