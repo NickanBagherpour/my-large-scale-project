e6e110d8b23e9895562f981dcf257f37ac936b85
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-19 11:32:10 +0330

    feat: work on validation schema

--- libs/backoffice/widgets/organization-management-widget/src/utils/consts.ts
+++ libs/backoffice/widgets/organization-management-widget/src/utils/consts.ts
@@ -31,3 +31,19 @@ export const FORM_ITEMS_NAME = {
     CLIENT_KEY: 'client_key',
   },
 };
+export const INPUTS_LENGTH = {
+  INQUERY_MAX_LENGTH: 11,
+  INPUTE_MAX_LENGTH: 255,
+  MAX_LEGAL_PERSON_NAME_LENGTH: 255,
+  MIN_LEGAL_PERSON_NAME_LENGTH: 3,
+  MAX_MOBILE_NUMBER_LENGTH: 11,
+  MAX_REGISTRATION_NUMBER_LENGTH: 11,
+  MAX_POSTAL_CODE_NUMBER_LENGTH: 10,
+  MAX_NATIONAL_ID_NUMBER_LENGTH: 11,
+  MAX_ECONOMY_CODE_NUMBER_LENGTH: 12,
+  MAX_LAST_REGISTRATION_ADDRESS_LENGTH: 150,
+};
+export const selectLegalTypeOptions: any[] = [
+  { label: 'عام', value: '1' },
+  { label: 'خاص', value: '2' },
+];
