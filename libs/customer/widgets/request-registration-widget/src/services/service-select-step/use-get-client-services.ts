bf854460e9e9659adfaf542ff67053ceed226eb9
ab.mohammadi <ab.mohammadi@sadad.co.ir>
2025-04-20 10:59:05 +0330

    refactor: use the ui-kit's AdvanceSelector

---
+++ libs/customer/widgets/request-registration-widget/src/services/service-select-step/use-get-client-services.ts
@@ -0,0 +1,35 @@
+import { withErrorHandling } from '@oxygen/utils';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { RQKEYS } from '@oxygen/utils';
+import Api from '../api';
+import { useAppDispatch } from '../../context';
+
+const PAGE_SIZE = 10;
+const SORT = 'createDate,DESC';
+
+export const useGetClientService = (query: string) => {
+  const dispatch = useAppDispatch();
+
+  return useInfiniteQuery({
+    queryKey: [RQKEYS.REUSABLE_COMPONENTS.CLIENT_SERVICES, query],
+    queryFn: ({ pageParam }) =>
+      withErrorHandling(
+        () =>
+          Api.getClientService({
+            query,
+            page: pageParam,
+            size: PAGE_SIZE,
+            sort: SORT,
+          }),
+        dispatch
+      )(),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      const { number, totalPages } = lastPage.page;
+      const isLastPage = number >= totalPages;
+
+      if (isLastPage) return null;
+      else return number + 1;
+    },
+  });
+};
