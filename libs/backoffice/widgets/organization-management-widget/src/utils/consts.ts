230fa28c77df5c6ad7a3fe6ea4c02f60575a3836
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-20 15:30:09 +0330

    feat: comment console log on client creation

--- libs/backoffice/widgets/organization-management-widget/src/utils/consts.ts
+++ libs/backoffice/widgets/organization-management-widget/src/utils/consts.ts
@@ -2,7 +2,6 @@ export const AVAILABLE_ROWS_PER_PAGE = [5, 10, 25, 50, 100];
 export const INITIAL_ROW_PER_PAGE = AVAILABLE_ROWS_PER_PAGE[0];
 export const INITIAL_PAGE = 1;
 
-export const INPUT_MAX_LENGTH = 100;
 export const INQUIRY_MAX_LENGTH = 11;
 
 export const FORM_ITEMS_NAME = {
