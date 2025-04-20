8c98c9f57e25b5e5e996d3ab284a61d278052716
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-20 08:57:45 +0330

    feat: handle the get organization 404 error status and 200 success status

--- libs/backoffice/widgets/organization-management-widget/src/services/get-organization-info.api.tsx
+++ libs/backoffice/widgets/organization-management-widget/src/services/get-organization-info.api.tsx
@@ -6,11 +6,21 @@ import Api from './api';
 import { useAppDispatch } from '../context';
 import { OrganizationInfoParamsType } from '../types/get-organization-info.type';
 
-export const useGetOrganizationInfoQuery = (params: OrganizationInfoParamsType) => {
+export const useGetOrganizationInfoQuery = (params: OrganizationInfoParamsType, setIsError) => {
   const dispatch = useAppDispatch();
   return useQuery({
     queryKey: [RQKEYS.BACKOFFICE.ORGANIZATION_MANAGEMENT.INQUERY],
-    queryFn: withErrorHandling(async () => Api.getOrganizationInfo(params), dispatch),
+    queryFn: async () => {
+      try {
+        const res = await withErrorHandling(() => Api.getOrganizationInfo(params), dispatch, {
+          ignore404Errors: true,
+        })();
+        return res;
+      } catch (e: any) {
+        setIsError(e?.response?.status);
+      }
+    },
+    // queryFn: withErrorHandling(async () => Api.getOrganizationInfo(params), dispatch),
     // throwOnError:(...e)=>{console.log(e)},
     enabled: false,
     networkMode: 'offlineFirst',
