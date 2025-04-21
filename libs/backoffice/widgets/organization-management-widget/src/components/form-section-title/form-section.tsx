bb7974937fc3313858d9c7fe48eb894098569223
a.ghaffar <a.ghaffar@sadad.co.ir>
2025-04-21 13:39:24 +0330

    feat: create and initial the form section component

--- libs/backoffice/widgets/organization-management-widget/src/components/form-section-title/form-section.tsx
+++ libs/backoffice/widgets/organization-management-widget/src/components/form-section-title/form-section.tsx
@@ -1,17 +1,25 @@
+import { ReactNode } from 'react';
 import { Tooltip } from '@oxygen/ui-kit';
 import { useAppTheme } from '@oxygen/hooks';
+import { useTr } from '@oxygen/translation';
 
 import * as S from './form-section.style';
 
-export const FormSection = ({ title, tooltip, children }) => {
+type FormSectionPropType = {
+  title: string;
+  tooltip?: string;
+  children: ReactNode;
+};
+export const FormSection = ({ title, tooltip, children }: FormSectionPropType) => {
   const theme = useAppTheme();
+  const [t] = useTr();
 
   return (
     <>
       <S.TitleContainer>
-        <S.TitleText>{title}</S.TitleText>
+        <S.TitleText>{t(title)}</S.TitleText>
         {tooltip && (
-          <Tooltip color={theme.primary.main} title={tooltip}>
+          <Tooltip color={theme.primary.main} title={t(tooltip)}>
             <S.Icon className={'icon-info-circle'} />
           </Tooltip>
         )}
