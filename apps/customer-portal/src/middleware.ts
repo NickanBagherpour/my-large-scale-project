60f358360b4ed206720e0212e687dd82225927d6
r.sheikholmoluki <r.sheikholmoluki@sadad.co.ir>
2025-04-21 10:18:51 +0330

    fix: public image

--- apps/customer-portal/src/middleware.ts
+++ apps/customer-portal/src/middleware.ts
@@ -18,7 +18,12 @@ export default async function middleware(request: NextRequest) {
   // const session = await auth();
   const token = request.cookies.get(CookieKey.SESSION_ID)?.value;
 
-  const publicPaths = [ROUTES.CUSTOMER.LANDING, ROUTES.CUSTOMER.AUTH, ROUTES.CUSTOMER.DOCS_API]; // Define public paths here
+  const publicPaths = [
+    ROUTES.CUSTOMER.LANDING,
+    ROUTES.CUSTOMER.AUTH,
+    ROUTES.CUSTOMER.DOCS_API,
+    '/assets/images/service-categories.jpg',
+  ]; // Define public paths here
 
   // Get the current pathname
   const { pathname } = request.nextUrl;
